a8dd1b4567cc710d0726ca19c9059a8028b28aae
Mikael Lund <mikael.lund@teokem.lu.se>
2007-10-19 10:52:02 +0000

    More biosim migration. Note that a new class has been added that can write xyz trajectory files understandable by VMD for example. However, this is an ascii format and takes up MUCH more space than the Gromacs XTC format as implemented in the new faunus framework (trunk/classes).

--- code-mikael/biosim/src/xyz.h
+++ code-mikael/biosim/src/xyz.h
@@ -13,18 +13,21 @@ class xyz {
     unsigned short fid;
     ofstream f;
     string base;
+    bool virgin;
   public:
-    xyz(string file="trajout") {
+    xyz(string file="trj") {
       fid=0;
       base=file;
       newfile();
     }
     void writetrj( vector<particle> &p, peptide &pep ) {
       f << p.size() << endl;
+      if (virgin==true) f << endl; // VMD hack
       for (int i=0; i<p.size(); i++)
         f << pep.d[p[i].id].name << " "
           << p[i].x << " " << p[i].y << " " << p[i].z << endl;
       f << endl;
+      virgin=false;
     }
     void newfile() {
       if (f) close();
@@ -33,6 +36,7 @@ class xyz {
       string s = base + suffix.str();
       f.open( s.c_str() );
       f.precision(3);
+      virgin=true;
     }
     void close() { f.close(); }
 };

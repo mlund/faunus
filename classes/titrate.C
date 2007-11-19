fd4af0d92cbdd488668cb5a3cc0a079eaaa9aa25
Bjorn Persson <bjorn.persson@teokem.lu.se>
2007-11-19 15:52:05 +0000

    GCtitrate is the heiress of titrate

--- classes/titrate.C
+++ classes/titrate.C
@@ -159,151 +159,21 @@ double titrate::applycharges(vector<particle> &p) {
  * \param spec Species class
  * \param peeage pH
  */
-GCtitrate::GCtitrate(species &spec, double peeage) {
-  ph=peeage;
-  spc=&spec;
+GCtitrate::GCtitrate(species &spec, double peeage, double cp, double vol) {
+  tit.ph=peeage;
+  tit.spc=&spec;
+  CatPot=cp;
+  volume=vol;
 };
-GCtitrate::GCtitrate(species &spec, vector<particle> &p, group &mobile, double peeage) {
-  ph=peeage;
-  spc=&spec;
-  init(p, mobile);
-}
-
-void GCtitrate::init(vector<particle> &p, group &mobile) {
-  for (int i=0; i<p.size(); i++)    //search for titrateable sites
-    if ( (*spc).d[p[i].id].pka !=0 ) {
-      sites.push_back(i);
-      p[i].charge = spc->d[p[i].id].p.charge; // deprotonate
-    }
-  for (int i=mobile.beg; i<mobile.end+1; i++) {
-    if (p[i].charge==+1) protons.push_back(i);
-    if (p[i].charge==0) neutrons.push_back(i);
-  }
-  q.resize( sites.size() ); //adjust site average charge vector
-}
-
-//returns number a random, titrateable
-//site in particle vector
-short int GCtitrate::random(vector<short int> &iv) {
-  short int i = rand() % iv.size();     //and pick randomly
-  return iv.at(i);
-}
-
-//Returns protonation status of particle i in particle vector
-  GCtitrate::keywords GCtitrate::status(vector<particle> &p, short int i) {
-    if ( p[i].charge > (*spc).d[p[i].id].p.charge )
-      return PROTONATED;
-    return DEPROTONATED;
-  }
-
-  GCtitrate::action GCtitrate::takeFromBulk(vector<particle> &p, short int i, short int j) {
-    if (j==-1)                               //if not specified..
-      j=random(protons);                     //..pick a random proton
-
-    protons.erase(remove(protons.begin(), protons.end(), j));
-
-    p[i].charge+=1;                            //protonate site.
-    p[j].charge=0;                             //neutralize proton...
-    neutrons.push_back(j);                     //..and add to neutron vector
-
-    action res;
-    res.action=PROTONATED;
-    res.site=i;
-    res.proton=j;
-    return res;
-  }
-
-  GCtitrate::action GCtitrate::moveToBulk(vector<particle> &p, short int i, short int j) {
-    if (j==-1)             // if not specified..
-      j=random(neutrons);  // ..pick random neutron
-    neutrons.erase(remove(neutrons.begin(), neutrons.end(), j)); //del a neutron
-
-    p[i].charge -= 1;      // deprotonate site.
-    p[j].charge  = 1;      // charge up neutron...
-    protons.push_back(j);  // ...add to proton vector
-
-    action res;
-    res.action=DEPROTONATED;
-    res.site=i;
-    res.proton=j;
-    return res;
-  };
-
-  GCtitrate::action GCtitrate::exchange(vector<particle> &p) {
-    int i=random( sites );        // pick a random, titrateable particle
-    if (status(p,i)==PROTONATED)
-      return moveToBulk(p,i);
-    else
-      return takeFromBulk(p,i);
-  }
-  GCtitrate::action GCtitrate::exchange(vector<particle> &p, action &a) {
-    if (status(p, a.site)==PROTONATED)
-      return moveToBulk(p, a.site, a.proton);
-    else
-      return takeFromBulk(p, a.site, a.proton);
-  };
-
-double GCtitrate::energy(vector<particle> &p,
-    double du, action &a) {
-  int i=p[a.site].id;
-  if (a.action==PROTONATED)
-    return du+( log(10.)*( ph - spc->d[i].pka ) );
-  else
-    return du-( log(10.)*( ph - spc->d[i].pka ) );
-}
-
-void GCtitrate::infos() {
-  cout << "# Titrateable sites   = " << sites.size() << endl
-    << "# Protons             = " << protons.size() << endl
-    << "# pH                  = " << ph << endl ;
-}
-
-double GCtitrate::sumsites() {
-  double Q=0;
-  for (int i=0; i<sites.size(); i++)
-    Q+= q[i].avg();
-  return Q;
-}
-
-void GCtitrate::samplesites(vector<particle> &p) {
-  for (int i=0; i<sites.size(); i++)
-    q[i] += p[sites[i]].charge;
-  nprot+=protons.size();
-}
-
-void GCtitrate::showsites(vector<particle> &p) {
-  cout << "# --- AVERAGE SITE CHARGES ---------------------\n";
-  for (int i=0; i<spc->d.size(); i++)
-    if (spc->d[i].pka!=0) {
-      cout << spc->d[i].name << ": " << setiosflags(ios::fixed);
-      cout.precision(3);
-      for (int j=0; j<sites.size(); j++)
-        if (p[sites[j]].id==i)
-          cout << q[j].avg() << " ";
-      cout << endl;
-    };
+GCtitrate::GCtitrate(species &spec, vector<particle> &p, group &mobile, double peeagei, double cp, double vol) {
+  tit.ph=peeage;
+  tit.spc=&spec;
+  tit.init(p, mobile);
+  volume=vol;
+  CatPot=cp;
 };
 
-/*!
- * This function will take the average charges from titrate::q
- * and apply these to the specified particle vector. Also it will
- * smear out charges on the protons.
- * WARNING! After this function you can no longer perform any
- * titration steps. It is meant to be called before saving coordinates
- * to disk, so as to include the partial charges of the system.
- */
-double GCtitrate::applycharges(vector<particle> &p) {
-  for (int i=0; i<sites.size(); i++)
-    p[sites[i]].charge = q[i].avg();
 
-  double q = nprot.avg() / protons.size();
-  for (int i=0; i<protons.size(); i++)
-    p[protons[i]].charge = q;
+void  GCtitration::updateVolume (double vol) {
 
-  cout << "# Charge, Titrateable sites = " << sumsites() << endl
-    << "# Charge, Protons           = " << nprot.avg() << endl
-    << "# Average proton charge     = " << q << endl
-    << "# Total                     = " << sumsites()+nprot.avg() << endl;
-
-  return sumsites();
 };
--- classes/titrate.h
+++ classes/titrate.h
@@ -49,38 +49,21 @@ class titrate {
     group sort(group &);
 };
 
+/*! \brief Class to preform grand canonical titration where the 
+ *         potential determining ion is coupeld with a bulk salt
+ *  \author Bjorn Persson/Mikael Lund
+ *  \todo Everything...
+ */
+
+
 class GCtitrate {
   public:
-    enum keywords {PROTONATED,DEPROTONATED,ANY,NOACID};
-    vector<short int> sites, protons, neutrons;
-    double ph, CatPot;                          //!< System pH, chemical potential of cation
+    double  CatPot, volume;         //!< Systems chemical potential of cation, system volume
+    titrate tit;
 
-    struct action {
-      keywords action;
-      short int site;        
-      short int proton;    
-    };
-
-    GCtitrate(species &, double);
-    GCtitrate(species &, vector<particle> &, group &, double);
-    void init(vector<particle> &, group &);//!< Locate and initialize sites and protons
-    action exchange(vector<particle> &);
-    action exchange(vector<particle> &, action &);
-    double sumsites();                     //!< Calculates total charge of titrateable sites
-    double energy(vector<particle> &, double, action &);
-    void samplesites(vector<particle> &);  //!< Updates the average charge vector titrate::q
-    void showsites(vector<particle> &);    //!< Print average charges of titrateable sites
-    double applycharges(vector<particle> &);//!< Copy average charges to particles in the particle vector
-  private:
-    void infos();
-    species *spc;
-    short int random(vector<short int> &); //!< Pick a random item in a vector
-    average<float> nprot;               //!< Average number of protons. Updated with titrate::samplesites
-    vector<average <float> >  q;        //!< Stores the average charges of sites from titrate::sites
-    action takeFromBulk(vector<particle> &, short int, short int=-1);
-    action moveToBulk(vector<particle> &, short int, short int=-1);
-    keywords status( vector<particle> &, short int );
-    group sort(group &);
+    GCtitrate GCtitrate(species &, double, double, double);//!< Constructor
+    GCtitrate GCtitrate(species &, vector<particle> &, group &, double, double, double); //!<Constroctor
+    void updateVolume( double );            //!< Update the system volume, for isobaric ensemble
 };
 
 #endif

e106aed259869bd1fd854cb24cd47ec7bbb38b67
Mikael Lund <mikael.lund@teokem.lu.se>
2007-11-19 11:22:14 +0000

    Moved rdf functions around

--- examples/twobody/twobody.C
+++ examples/twobody/twobody.C
@@ -15,7 +15,7 @@
 #include "countdown.h"
 #include "histogram.h"
 #include "inputfile.h"
-typedef pot_minimage T_pairpot;          // Specific pair interaction function
+typedef pot_minimage T_pairpot;         // Specific pair interaction function
 #include "markovmove.C"
 
 using namespace std;
@@ -23,7 +23,7 @@ using namespace std;
 int main() {
   inputfile in("twobody.conf");         // Read input file
   slump slump;                          // A random number generator
-  box cell(90.);                       // We want a spherical cell
+  box cell(90.);                        // We want a cubic cell
   canonical nvt;                        // Use the canonical ensemble
   pot_setup cfg;                        // Setup pair potential (default values)
   cfg.box = cell.len;
@@ -40,7 +40,7 @@ int main() {
         aam.load("mrh4a.aam"), true );  // ...positions.
 
   group salt;                           // Group for mobile ions
-  salt.add( cell, particle::NA, 0+880);// Insert sodium ions
+  salt.add( cell, particle::NA, 0+880); // Insert sodium ions
   salt.add( cell, particle::SO4,30+440);// Insert chloride ions
   saltmove sm(nvt, cell, pot);          // Class for salt movements
   macrorot mr(nvt, cell, pot);          // Class for macromolecule rotation
@@ -53,37 +53,38 @@ int main() {
   #endif
 
   for (int macro=1; macro<=10; macro++) {       // Markov chain 
-    for (int micro=1; micro<=2e5; micro++) {
+    for (int micro=1; micro<=6e4; micro++) {
 
       sys+=sm.move(salt);                       // Displace salt particles
-      saltrdf.update(cell);
       for (int i=0; i<g.size(); i++) {          // Loop over proteins
         if (slump.random_one()<0.5)
           sys+=mr.move(g[i]);                   // Rotate...
         else {
           sys+=mt.move(g[i]);                   // ...or translate
-          for (int j=0; j<g.size(); j++)
+          for (int j=0; j<g.size(); j++)        // Analyse protein g(r)
             if (j!=i)
-              protrdf.update(cell,g[i].cm,g[j].cm); // Update mass center g(r)
+              protrdf.update(cell,g[i].cm,g[j].cm);
         }
       }
 
-      if (slump.random_one()>0.8) {
-        sm.adjust_dp(60,70);
-        mr.adjust_dp(60,70);
+      if (slump.random_one()>0.9) {
+        saltrdf.update(cell);                   // Analyse salt g(r)
+        sm.adjust_dp(60,70);                    // Tune displacement
+        mr.adjust_dp(60,70);                    // ...parameters
         mt.adjust_dp(60,70);
         #ifdef GROMACS
-        xtc.save("ignored-name.xtc", cell.p);
+        xtc.save("ignored-name.xtc", cell.p);   // Save trajectory
         #endif
       }
     } // End of inner loop
+
     cout << "Macro step " << macro << " completed. ETA: " << clock.eta(macro);
     sys.update(pot.energy(cell.p));             // Update system energy averages
     cell.check_vector();                        // Check sanity of particle vector
 
-    xyz.save("coord.xyz", cell.p);
-    protrdf.write("rdfprot.dat");
-    saltrdf.write("rdfsalt.dat");
+    xyz.save("coord.xyz", cell.p);              // Write .xyz coordinate file
+    protrdf.write("rdfprot.dat");               // Write g(r)'s
+    saltrdf.write("rdfsalt.dat");               //   -//-
 
   } // End of outer loop
 

a09f965a6bad76cb18d6900b6468a51472571a63
Mikael Lund <mikael.lund@teokem.lu.se>
2007-12-04 14:33:21 +0000

    Manus update -- changed source

--- examples/titration/pka.C
+++ examples/titration/pka.C
@@ -12,43 +12,43 @@ using namespace std;
 int main() {
   cell::cell con(100.);                 // Use a spherical container
   canonical nvt;                        // Use the canonical ensemble
-  pot_setup cfg;                        // Setup pair potential (default values)
+  pot_setup cfg;                        // Setup pair potential (default)
   interaction<T_pairpot> pot(cfg);      // Functions for interactions
   countdown<int> clock(10);             // Estimate simulation time
   macromolecule protein;                // Group for the protein
   ioaam aam(con);                       // Protein input file format is AAM
   protein.add( con, aam.load(
         "calbindin.aam" ) );            // Load protein from disk
-  protein.move(con, -protein.cm);       // ..and translate it to origo (0,0,0)
+  protein.move(con, -protein.cm);       // ..translate it to origo (0,0,0)
   protein.accept(con);                  // ..accept translation
   group salt;                           // Group for salt and counter ions
-  salt.add( con, particle::NA, 34+19);  // Insert sodium ions
-  salt.add( con, particle::CL, 34 );    // Insert chloride ions
+  salt.add( con, particle::NA, 34+19);  //   Insert sodium ions
+  salt.add( con, particle::CL, 34 );    //   Insert chloride ions
   saltmove sm(nvt, con, pot);           // Class for salt movements
   aam.load(con, "confout.aam");         // Load old config (if present)
   chargereg tit(nvt,con,pot,salt,7.6);  // Prepare titration. pH 7.6
-  systemenergy sys(pot.energy(con.p));  // System energy analysis (drift etc)
+  systemenergy sys(pot.energy(con.p));  // System energy analysis
   cout << con.info() << tit.info();     // Some information
 
-  for (int macro=1; macro<=10; macro++) {      // Markov chain
+  for (int macro=1; macro<=10; macro++) {    // Markov chain
     for (int micro=1; micro<=1e3; micro++) {
-      switch (rand() % 2) {                    // Randomly chose move
+      switch (rand() % 2) {                  // Randomly chose move
         case 0:
-          sys+=sm.move(salt);                  // Displace salt particles
+          sys+=sm.move(salt);                // Displace salt particles
           break;
         case 1:
-          sys+=tit.titrateall();               // Titrate sites on the protein
-          protein.charge(con.p);               // Re-calc. protein charge
-          protein.dipole(con.p);               // Re-calc. dipole moment
+          sys+=tit.titrateall();             // Titrate sites on the protein
+          protein.charge(con.p);             // Re-calc. protein charge
+          protein.dipole(con.p);             // Re-calc. dipole moment
           break;
       }
-    }                                          // END of micro loop
-    sys.update(pot.energy(con.p));             // Update system energy averages
-    aam.save("confout.aam", con.p);            // Save configuration to disk
-    cout << "Macro step " << macro << " completed. ETA: "
-         << clock.eta(macro);
-  }                                            // END of macro loop
-  cout << sys.info() << sm.info() << tit.info()// Print results
-       << salt.info() << protein.info();
+    }                                        // END of micro loop
+    sys.update(pot.energy(con.p));           // Update system energy averages
+    aam.save("confout.aam", con.p);          // Save configuration to disk
+    cout << "Macro step " << macro
+         << " completed. ETA: " << clock.eta(macro);
+  }                                          // END of macro loop
+  cout << sys.info() << sm.info()            // Print results
+       << tit.info() << salt.info() << protein.info();
 }
 
---
+++ misc/manus/pics/gensource.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+enscript -o s.ps -E -B -2 -r --font=Courier6 -M Env10 -L 26 $1
+
+

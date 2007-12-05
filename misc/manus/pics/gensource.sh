a77e82f73f7071f2dbaf4ff0ad7d3bdfd9d83d7c
Mikael Lund <mikael.lund@teokem.lu.se>
2007-12-05 15:00:24 +0000

    Source code example now in color

--- misc/manus/pics/gensource.sh
+++ misc/manus/pics/gensource.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-enscript -C -o s.ps -E -B -2r --font=CourierBold6 -M Env10 -L 26 $1
+enscript --color -C -o s.ps -E -B -2r --font=CourierBold6 -M Env10 -L 26 $1
+ps2pdf14 s.ps
+pdfcrop s.pdf
 
 

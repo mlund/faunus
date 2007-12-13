643af7560c4e3a20ab1b91c9fc3b41a2c492d742
Mikael Lund <mikael.lund@teokem.lu.se>
2007-12-13 14:04:54 +0000

    Take hydrophobic LJ scaling from inputfile

--- classes/pot_hydrophobic.h
+++ classes/pot_hydrophobic.h
@@ -3,23 +3,29 @@
 
 #include "potentials.h"
 
-/*! \brief Double vdW interaction for iodide and hydrophobic  groups 
- *  \author Mikael Lund
+/*! \brief Double vdW interaction for certain particles with hydrophobic groups 
+ *  \author Mikael Lund 
+ *
+ *  This potential consists of a Coulomb and LJ potential as usual. When encountering
+ *  a particle of type "id" AND a hydrophobic group, the LJ epsilon parameter will be
+ *  scaled by pot_setup::hydroscale (Input keyword: "hydroscale").
  */
 class pot_hydrophobic : private pot_lj {
   private:
     double scale;
   public:
-    double f;             //!< Factor to convert to kT
+    double f;             //!< Factor to convert returned energy to kT
+    particle::type id;    //!< Particle to interact with hydrophobic groups (default: iodide)
     pot_hydrophobic( pot_setup &pot ) : pot_lj( pot.eps/pot.lB ) {
       f=pot.lB;
       scale=pot.hydroscale;
+      id=particle::I;
     }
     inline double pairpot(particle &p1, particle &p2) {
       register double r2=p1.sqdist(p2), u=lj(p1,p2,r2);
-      if (p1.id==particle::I && p2.hydrophobic==true)
+      if (p1.id==id && p2.hydrophobic==true)
         u=scale*u;
-      if (p2.id==particle::I && p1.hydrophobic==true)
+      if (p2.id==id && p1.hydrophobic==true)
         u=scale*u;
       return u + p1.charge*p2.charge/sqrt(r2);
     }
@@ -34,5 +40,4 @@ string pot_hydrophobic::info() {
     << "#   Hydrop. LJ scaling = " << scale << endl;
   return o.str();
 }
-
 #endif

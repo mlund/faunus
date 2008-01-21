299b2e587c6ea1f01d4193d4819ca20e3bf15591
Bjorn Persson <bjorn.persson@teokem.lu.se>
2008-01-21 16:06:47 +0000

    Fixed some errors

--- examples/twobody/twobody.C
+++ examples/twobody/twobody.C
@@ -9,54 +9,53 @@
  */
 
 #include <iostream>
-#include "io.h"
 #include "analysis.h"
-#include "potentials.h"
-#include "container.h"
-#include "countdown.h"
-#include "histogram.h"
-#include "inputfile.h"
+#include "mcloop.h"
 typedef pot_coulomb T_pairpot;         // Specific pair interaction function
 #include "markovmove.h"
 
 using namespace std;
 
 int main() {
-  enum dstkeys { Q1=0,Q2,MU1,MU2, LAST };
   slump slump;                          // A random number generator
   inputfile in("twobody.conf");         // Read input file
+  mcloop loop(in);                      // Set Markov chain loop lengths
   cell cell(in);                        // We want a spherical, hard cell
   canonical nvt;                        // Use the canonical ensemble
   pot_setup cfg(in);                    // Setup pair potential (default values)
   interaction<T_pairpot> pot(cfg);      // Functions for interactions
-  countdown<int> clock(10);             // Estimate simulation time
   ioxyz xyz(cell);                      // xyz output for VMD etc.
-  distributions dst(LAST,0,180);
-  rdf saltrdf(particle::NA,particle::SO4, .5, cell.r);
+  distributions dst;                    // Distance dep. averages
+  rdf saltrdf(particle::NA,particle::CL, .5, cell.r);
 
   vector<macromolecule> g;              // Group for proteins
   dualmove dm(nvt, cell, pot);          //   Class for 1D macromolecular translation
   dm.load( in, g, 40.);                 //   Load proteins and separate them 
+  dm.rmax=60;
   salt salt;                            // Group for mobile ions
   salt.add(cell, in);                   //   Add salt particles
   saltmove sm(nvt, cell, pot);          // Class for salt movements
   macrorot mr(nvt, cell, pot);          // Class for macromolecule rotation
 
+  dm.dp=6;                              // Set displacement parameters
+  sm.dp=90;                             // Set displacement parameters
+
   ioaam aam(cell);                      // Protein input file format is AAM
   if (aam.load(cell,"confout.aam")) {
-    g[0].masscenter(cell.p);            // Load old config (if present)
-    g[1].masscenter(cell.p);            // ...and recalc mass centers
+    g[0].masscenter(cell);              // Load old config (if present)
+    g[1].masscenter(cell);              // ...and recalc mass centers
   }
-  chargereg tit(nvt,cell,pot,salt,5.0); // Prepare titration.
+  chargereg tit(nvt,cell,pot,salt,4.0); // Prepare titration.
   systemenergy sys(pot.energy(cell.p)); // System energy analysis
-  cout << cell.info() << pot.info();    // Print information to screen
+  cout << cell.info() << pot.info()     // Print information to screen
+    << tit.info();
 
   #ifdef GROMACS
   ioxtc xtc(cell, cell.r);              // Gromacs xtc output (if installed)
   #endif
 
-  for (int macro=1; macro<=10; macro++) {       // Markov chain 
-    for (int micro=1; micro<=2e5; micro++) {
+  for (int macro=1; macro<=loop.macro; macro++) {//Markov chain 
+    for (int micro=1; micro<=loop.micro; micro++) {
       short i,n;
       switch (rand() % 4) {                     // Pick a random MC move
         case 0:                                 // Displace salt
@@ -73,10 +72,12 @@ int main() {
           break;
         case 3:                                 // Fluctuate charges
           sys+=tit.titrateall();                // Titrate sites on the protein
-          dst.add(Q1,dm.r, g[0].charge(cell.p));// Re-calc. protein charges
-          dst.add(Q2,dm.r, g[1].charge(cell.p));
-          dst.add(MU1,dm.r,g[0].dipole(cell.p));// ..and dipole moments
-          dst.add(MU2,dm.r,g[1].dipole(cell.p));
+          if (tit.du!=0) {                      // Average charges and dipoles
+            dst.add("Q1",dm.r, g[0].charge(cell.p));
+            dst.add("Q2",dm.r, g[1].charge(cell.p));
+            dst.add("MU1",dm.r,g[0].dipole(cell.p));
+            dst.add("MU2",dm.r,g[1].dipole(cell.p));
+          }
           break;
       }
       if (slump.random_one()>.8 && macro>1)
@@ -86,19 +87,17 @@ int main() {
       if (slump.random_one()>.95 && macro>1)
         xtc.save("ignored-name.xtc", cell.p);   // Save trajectory
       #endif
-
     } // End of inner loop
 
     sys.update(pot.energy(cell.p));             // Update system energy averages
     cell.check_vector();                        // Check sanity of particle vector
 
+    dm.gofr.write("rdfprot.dat");               // Write interprotein g(r)
+    saltrdf.write("rdfsalt.dat");               // Write salt g(r)
+    dst.write("distributions.dat");             // Write other distributions
     xyz.save("coord.xyz", cell.p);              // Write .xyz coordinate file
     aam.save("confout.aam", cell.p);            // Save config. for next run
-    dm.gofr.write("rdfprot.dat");
-    saltrdf.write("rdfsalt.dat");
-    cout << "Macro step " << macro
-         << " completed. ETA: " << clock.eta(macro);
- 
+    cout << loop.timing(macro);                 // Show progress
   } // End of outer loop
 
   cout << salt.info(cell)                       // Final information...

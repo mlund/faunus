14c4030cc9507766d5f8e6a5eca9501d3b73046a
Mikael Lund <mikael.lund@teokem.lu.se>
2008-08-05 20:36:24 +0000

    More babel support; cmake detection, file reader.

--- classes/iobabel.C
+++ classes/iobabel.C
@@ -1,14 +1,23 @@
 #include "iobabel.h"
-
 void iobabel::read(string filename, string format) {
+  p.clear();
+  mol.Clear();
   obconv.SetInFormat(format.c_str());
-  bool notatend = obconv.ReadFile(&mol, filename.c_str());
+  bool notatend = obconv.ReadFile(&mol,filename.c_str());
   while (notatend) {
-    cout << "Molecular Weight: " << mol.GetMolWt() << endl
-      << "Residues = " << mol.NumResidues() << endl
-      << "Atoms    = " << mol.NumAtoms() << endl
-      << "NumRotors= " << mol.NumRotors() << endl;
+    for (unsigned int i=1; i<=mol.NumAtoms(); i++)
+      p.push_back(get(i));
     mol.Clear();
     notatend = obconv.Read(&mol);
   }
+}
+particle iobabel::get(unsigned int i) {
+  atom = mol.GetAtom(i);
+  v=atom->GetVector();
+  v.Get(c);
+  a.x=c[0]; a.y=c[1]; a.z=c[2];
+  a.mw=atom->GetAtomicMass();
+  if (a.mw==0) a.mw=1;  //we don't like weightless atoms.
+  a.charge=atom->GetPartialCharge();
+  return a;
 }
--- classes/iobabel.h
+++ classes/iobabel.h
@@ -9,17 +9,26 @@
 
 using namespace OpenBabel;
 
-/*! \brief OpenBabel file import/export
+/*! \brief OpenBabel file support
  *  \author Mikael Lund
- *  \todo Unfinished!
+ *  \date Helsingor, 2008
+ *
+ *  This class offers support for OpenBabel - a set of programs and
+ *  libraries for reading and writing a large number of molecular formats.
+ *  The main purpose of this class is to bridge between the babel api and
+ *  the faunus particle class approach.
  */
 class iobabel {
   private:
     OBConversion obconv;
     OBMol mol;
-    vector<particle> p;
+    OBAtom *atom;
+    particle a;
+    vector3 v;    // OpenBabel vector
+    double c[3];  // Temp. vector storage
   public:
-    void read(string,string);
+    vector<particle> p;         //!< Placeholder for loaded data
+    particle get(unsigned int); //!< Convert n'th babel atom to a particle
+    void read(string,string);   //!< Read entire file
 };
-
 #endif

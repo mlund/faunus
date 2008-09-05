e90610b4a1e4da2738ea6c76f6e6a0e9fd8323c0
Mikael Lund <mikael.lund@teokem.lu.se>
2008-09-05 14:06:48 +0000

    Added fortran support to cmake. Exemplified for the "bulk.f" program.

--- legacy/bulk/compile.sh
+++
@@ -1,8 +0,0 @@
-# Generic linux compile
-ifort -O3 -o bulk.run bulk.f
-
-#fort77 -O2 -funroll-loops -finit-local-zero -fno-automatic -o bulk.run bulk.f
-
-#Compile options for IRIX/SGI
-#f77  -Ofast -static -OPT:IEEE_arithmetic=3:roundoff=3 -n32 -o bulk.run bulk.f
-

f9b6c2abf928725e69e3c9704649bc5000047c31
Mikael Lund <mikael.lund@teokem.lu.se>
2008-09-12 20:09:39 +0000

    cmake cleanup and preparetion for boost-python. xcode project deleted as it can be created with cmake.

---
+++ cmake/modules/OptimizationFlags.cmake
@@ -0,0 +1,21 @@
+# Compiler specific optimization flags.
+# - Set OPENMP to test and enable OpenMP support
+# $Mikael Lund, 2008
+
+function( fau_setcmp cid flags fomp )
+if (CMAKE_CXX_COMPILER_ID MATCHES "${cid}")
+  set (DUMMY "${flags}" PARENT_SCOPE)
+  CHECK_CXX_ACCEPTS_FLAG( ${fomp} OMP_CHECK)
+  if (OPENMP AND OMP_CHECK)
+    set(DUMMY "${flags} ${fomp}" PARENT_SCOPE)
+  endif(OPENMP AND OMP_CHECK)
+endif (CMAKE_CXX_COMPILER_ID MATCHES "${cid}")
+set (CMAKE_CXX_FLAGS_RELEASE "${DUMMY}" CACHE STRING "C++ compiler flags (automatic)" FORCE)
+endfunction( fau_setcmp )
+
+fau_setcmp("GNU" "-O3 -w -funroll-loops -DNDEBUG" "-fopenmp" ) # GNU
+fau_setcmp("Intel" "-O3 -w -DNDEBUG" "-openmp")  # Intel
+fau_setcmp("Sun" "-fast -w -DNDEBUG" "-xopenmp") # Sun
+fau_setcmp("pgCC" "-O3 -w -DNDEBUG" "-mp")       # Portland
+fau_setcmp("pathCC" "-Ofast -DNDEBUG" " ")       # Pathscale
+

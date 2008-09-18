febd5d3c3e3482c09819a46f68245559cc85d28b
Ondrej Marsalek <ondrej.marsalek@uochb.cas.cz>
2008-09-18 12:56:39 +0000

    cmake: main CMakeLists.txt cleanup
    python: proof-of-concept wrapper created, uses the Boost Python library

---
+++ undone/python-proof-of-concept/pyfaunus.cpp
@@ -0,0 +1,47 @@
+#include <boost/python.hpp>
+#include "faunus/point.h"
+#include "faunus/potentials.h"
+
+namespace Faunus {
+    typedef Faunus::interaction<Faunus::pot_coulomb> interaction_coulomb;
+}
+
+BOOST_PYTHON_MODULE( pyfaunus ) {
+
+    using namespace boost::python;
+
+/*    def("greet", greet);
+
+    def("fn", fn);
+
+    def("mod", mod);
+
+    class_<VerySimple>("VerySimple")
+        .def_readwrite("a", &VerySimple::a)
+    ;
+
+    class_<Simple>("Simple")
+        .def_readwrite("b", &Simple::b)
+        .def_readwrite("v", &Simple::v)
+    ;
+
+    class_< Test<Simple> >("TestSimple")
+        .def_readwrite("data", &Test<Simple>::data)
+    ;
+*/
+
+    class_<Faunus::point>("point")
+        .def_readwrite("x", &Faunus::point::x)
+        .def_readwrite("y", &Faunus::point::y)
+        .def_readwrite("z", &Faunus::point::z)
+        .def("clear", &Faunus::point::clear)
+    ;
+
+    class_<Faunus::pot_setup>("pot_setup")
+    ;
+
+    class_<Faunus::interaction_coulomb>("interaction_coulomb", init<Faunus::pot_setup&>())
+        .def("dipdip", &Faunus::interaction_coulomb::dipdip)
+    ;
+}
+

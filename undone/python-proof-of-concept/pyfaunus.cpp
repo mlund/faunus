cd508e605be24913736970d32a01a5981b30fd99
Mikael Lund <mikael.lund@teokem.lu.se>
2008-09-20 12:05:03 +0000

    pyfaunus: fixed?

--- undone/python-proof-of-concept/pyfaunus.cpp
+++ undone/python-proof-of-concept/pyfaunus.cpp
@@ -37,12 +37,11 @@ BOOST_PYTHON_MODULE( pyfaunus ) {
         .def_readwrite("z", &Faunus::point::z)
         .def("clear", &Faunus::point::clear)
     ;
-/*
-    class_<Faunus::inputfile>("inputfile")
+    class_<Faunus::inputfile>("inputfile", init<std::string>())
     ;
+ 
     class_<Faunus::interaction_coulomb>("interaction_coulomb", init<Faunus::inputfile&>())
         .def("dipdip", &Faunus::interaction_coulomb::dipdip)
     ;
-    */
 }
 

8b8879be5d9a1456428852fbb55e2cbde6c0e3e8
Mikael Lund <mikael.lund@teokem.lu.se>
2009-03-26 20:32:45 +0000

    Added gccxml cvs install script

---
+++ misc/python/install_gccxml.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cvs=$1
+prefix=`pwd`/opt
+
+# checkout from cvs
+if [ ! -e gccxml ]
+then
+  echo "Enter blank password when prompted."
+  $cvs -d :pserver:anoncvs@www.gccxml.org:/cvsroot/GCC_XML login
+  $cvs -d :pserver:anoncvs@www.gccxml.org:/cvsroot/GCC_XML co gccxml
+fi
+
+# build and install
+if [ -e gccxml ]
+then
+  tmpdir=gccxml-build
+  mkdir $tmpdir
+  cd $tmpdir
+  cmake ../gccxml -DCMAKE_INSTALL_PREFIX:PATH=${prefix}
+  make -j2
+  make install
+  cd ..
+  rm -fR $tmpdir
+fi

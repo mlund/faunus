67d5c48ea219c3dfebbb6d3f8c1bce64d2a363bd
Mikael Lund <mikael.lund@teokem.lu.se>
2009-03-26 15:20:22 +0000

    SVN revision info now included in splash() function. Python README detailing mac-specific issues/fix.

---
+++ cmake/modules/SVNinfo.cmake
@@ -0,0 +1,23 @@
+#
+# Cmake script to extract the Subversion revision
+# of the current directory. Zero if returned of the 
+# dir is not under version control.
+# Mikael Lund, 2009
+#
+find_package(Subversion)
+if (Subversion_FOUND)
+  set(SVNREVISION 0)
+  execute_process(
+    COMMAND ${Subversion_SVN_EXECUTABLE} info
+    RESULT_VARIABLE SVNRC
+    OUTPUT_VARIABLE SVNOUT
+    ERROR_QUIET )
+  if(SVNRC EQUAL 0)
+    execute_process(
+      COMMAND echo "${SVNOUT}"
+      COMMAND grep "Revision: "
+      COMMAND awk "{print $2}"
+      OUTPUT_VARIABLE SVNREVISION
+      ERROR_QUIET )
+  endif(SVNRC EQUAL 0)
+endif (Subversion_FOUND)

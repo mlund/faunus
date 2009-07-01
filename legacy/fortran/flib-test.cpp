0cd3abb14fbe8ab490ffb26a4ceab6a6f7e70ec3
Mikael Lund <mikael.lund@teokem.lu.se>
2009-07-01 15:41:28 +0000

    More fortran function library stuff

--- legacy/fortran/flib-test.cpp
+++ legacy/fortran/flib-test.cpp
@@ -1,5 +1,9 @@
+// Link against libfaunus and libfortranfunc
 #include "faunus/fortran.h"
 int main() {
+  // See: http://www.yolinux.com/TUTORIALS/LinuxTutorialMixingFortranAndC.html
+  double x[10];
+  int n=2;
   Faunus::gentab_();
-
+  Faunus::vscoul_(x,x,x,&n);
 }

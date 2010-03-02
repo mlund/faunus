b2f579e3ae0a38337aefa5d9880671a90c83b838
Mikael Lund <mikael.lund@teokem.lu.se>
2010-03-02 17:26:08 +0000

    Year update and added Anil to splash message

--- src/faunus.cpp
+++ src/faunus.cpp
@@ -5,7 +5,7 @@ namespace Faunus {
     std::ostringstream o;
     o << "# ---------------------------------------------------------------------\n"
       << "#  Welcome to FAUNUS - A Framework for Molecule Simulation.\n"
-      << "#  Copyright (C) 2002-2009 Mikael Lund\n"
+      << "#  Copyright (C) 2002-2010 Mikael Lund\n"
       << "#\n"
       << "#  This program is free software; you can redistribute it and/or modify\n"
       << "#  it under the terms of the GNU General Public License as published by\n"
@@ -14,7 +14,8 @@ namespace Faunus {
       << "#\n"
       << "#  Programmed by:\n"
       << "#     Mikael Lund, Bjorn Persson, Martin Trulsson,\n"
-      << "#     Ondrej Marsalek, Christophe Labbez, Andre Teixeira.\n"
+      << "#     Ondrej Marsalek, Christophe Labbez, Andre Teixeira,\n"
+      << "#     Anil Kurut.\n"
       << "#\n"
       << "#  Reference:\n"
       << "#     Source Code Biol. Med. (2008) 3:1\n"

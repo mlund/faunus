7bb829bc8bfe364b73de2f959cabe1db0eb036f4
Mikael Lund <mikael.lund@teokem.lu.se>
2010-06-26 20:44:29 +0000

    CMake configuration cleanup. Fixed SVN error when building exported repositories. Simpler configuration of openmp/optimization flags - debug/release can now be selected in cmake.

--- cmake/modules/OptimizationFlags.cmake
+++ cmake/modules/OptimizationFlags.cmake
@@ -1,33 +1,26 @@
 # Compiler specific optimization flags.
-# - Set ENABLE_OPENMP to test and enable OpenMP support
+# - Set ENABLE_OPENMP to enable OpenMP support
 # $Mikael Lund, 2008
+# See http://fedetft.wordpress.com/2009/12/21/cmake-part-2-compiler-flags/
 
-include(TestCXXAcceptsFlag)
+unset(CMAKE_CXX_FLAGS)
 
-function( fau_setcmp cxxid flags fomp )
+if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+  set(CMAKE_CXX_FLAGS "-funroll-loops -W -Wno-sign-compare -Wno-conversion")
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+  set(CMAKE_CXX_FLAGS "-w1 -Wno-unknown-pragmas")
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "CLang")
+  set(CMAKE_CXX_FLAGS "-Wno-unused-parameter")
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "Sun")
+  set(CMAKE_CXX_FLAGS "")
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "PGI")
+  set(CMAKE_CXX_FLAGS "")
+elseif (CMAKE_CXX_COMPILER_ID MATCHES "pathCC")
+  set(CMAKE_CXX_FLAGS "")
+endif()
 
-if ( NOT ENABLE_OPENMP STREQUAL ENABLE_OPENMP_BAK )
-  set (SKIP_COMPILER_CHECK CACHE INTERNAL "Skip Compiler Test")
-endif ( NOT ENABLE_OPENMP STREQUAL ENABLE_OPENMP_BAK )
+if (ENABLE_OPENMP AND OPENMP_FOUND)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+endif()
 
-if (NOT SKIP_COMPILER_CHECK)
-  if (CMAKE_CXX_COMPILER_ID MATCHES "${cxxid}")
-    set (newflags "${flags}")
-    CHECK_CXX_ACCEPTS_FLAG( ${fomp} OMP_CHECK)
-    if (ENABLE_OPENMP AND OMP_CHECK)
-      set(newflags "${newflags} ${fomp}")
-    endif(ENABLE_OPENMP AND OMP_CHECK)
-    set (CMAKE_CXX_FLAGS_RELEASE "${newflags}" CACHE STRING "C++ compiler flags (automatic)" FORCE)
-    set (SKIP_COMPILER_CHECK ON CACHE INTERNAL "Skip Compiler Test")
-    set (ENABLE_OPENMP_BAK ${ENABLE_OPENMP} CACHE INTERNAL "ENABLE_OPENMP-STORAGE")
-  endif (CMAKE_CXX_COMPILER_ID MATCHES "${cxxid}")
-endif (NOT SKIP_COMPILER_CHECK)
-endfunction( fau_setcmp )
-
-fau_setcmp("GNU" "-O3 -funroll-loops -W -Wno-sign-compare -Wno-conversion -DNDEBUG" "-fopenmp" ) # GNU
-fau_setcmp("Intel" "-O3 -w1 -Wno-unknown-pragmas -DNDEBUG" "-openmp") # Intel
-fau_setcmp("Sun" "-fast -DNDEBUG" "-xopenmp")    # Sun
-fau_setcmp("PGI" "-O3 -DNDEBUG" "-mp")           # Portland
-fau_setcmp("pathCC" "-Ofast -DNDEBUG" " ")       # Pathscale
-fau_setcmp("CLang" "-O3 -Wno-unused-parameter -DNDEBUG" "-fopenmp")    # clang++
-
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "Additional compilation flags" FORCE)

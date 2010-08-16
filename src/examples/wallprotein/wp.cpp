cbbc0211b96c76e1c9272d8d0ca7003ffa4c1306
Mikael Lund <mikael.lund@teokem.lu.se>
2010-08-16 18:25:25 +0000

    Fixed wp_noslit target so that it actually does NOT have a slit.

--- src/examples/wallprotein/wp.cpp
+++ src/examples/wallprotein/wp.cpp
@@ -19,7 +19,7 @@ using namespace Faunus;
 
 #ifdef NOSLIT
 	typedef pot_r12minimage Tpot;
-	typedef slit Tcon;
+	typedef box Tcon;
 #else
 	typedef pot_r12minimageXY Tpot;
 	typedef slit Tcon;
@@ -32,9 +32,15 @@ int main() {
   mcloop loop(in);
   grandcanonical nmt;
   Tcon con(in);
+  double* zhalfPtr;
+#ifdef NOSLIT
+  zhalfPtr=&con.len_half;
+#else
+  zhalfPtr=&con.zlen_half;
+#endif
   springinteraction<Tpot> pot(in);
   distributions dst;    // Distance dep. averages
-  histogram gofr(0.1,0,con.zlen);
+  histogram gofr(0.1,0, (*zhalfPtr)*2. );
 
   // Handle polymers
   polymer pol;
@@ -52,7 +58,6 @@ int main() {
   mt.dpv.x=mt.dpv.y=0;            // ...no need to translate in xy direction
   cout << pol.info();
   
-
 #ifdef NOSLIT
   mr.runfraction=0;
   mr.dp=0;
@@ -62,7 +67,7 @@ int main() {
   group wall;
   wall.add(con, atom[in.getstr("wall_tion1")].id, in.getint("wall_nion1") );
   for (int i=wall.beg; i<=wall.end; i++)
-    con.p[i].z=con.zlen_half; // move all particles to edge
+    con.p[i].z=*zhalfPtr; // move all particles to edge
   con.trial=con.p;
   saltmove wm(nmt,con,pot,in,"wall_");
   wm.name="WALL PARTICLE DISPLACEMENTS";
@@ -117,18 +122,18 @@ int main() {
           for (int i=0; i<pol.size(); i++)
             sys+=mm.move(pol);   // move monomers
           pol.masscenter(con);
-          gofr.add(con.zlen_half-pol.cm.z);
+          gofr.add(*zhalfPtr-pol.cm.z);
           break;
         case 3:
           sys+=mt.move(pol);     // translate polymers
-          gofr.add(con.zlen_half-pol.cm.z);
+          gofr.add(*zhalfPtr-pol.cm.z);
           break;
         case 4:
           sys+=mr.move(pol);     // rotate polymers
           break;
         case 5:
           sys+=cs.move(pol);     // crankshaft
-          gofr.add(con.zlen_half-pol.cm.z);
+          gofr.add(*zhalfPtr-pol.cm.z);
           break;
         case 6:
           sys+=sb.move();        // grand canonical salt move
@@ -141,13 +146,13 @@ int main() {
 
       if (slp.random_one()>0.3) {
         pol.masscenter(con);
-        double z=con.zlen_half-pol.cm.z;
+        double z=*zhalfPtr-pol.cm.z;
         dst.add("qtot", z, pol.charge(con.p));
         dst.add("Rg2", z, pol.sqmassgradius(con));
         dst.add("Ree2", z, pol.sqend2enddistance(con)); 
         for (int i=pol.beg; i<=pol.end; i++) {
           std::ostringstream s; s << "q" << i;
-          dst.add( s.str(), con.zlen_half-con.p[i].z, con.p[i].charge );	
+          dst.add( s.str(), *zhalfPtr-con.p[i].z, con.p[i].charge );	
         }
       }
 

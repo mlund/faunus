1fbb4c48b2b10f1ae0fbafc1082946bd2839aad6
Mikael Lund <mikael.lund@teokem.lu.se>
2011-08-16 19:34:45 +0000

    branch ny - group implementation

--- include/faunus/group.h
+++ include/faunus/group.h
@@ -6,28 +6,37 @@
 namespace Faunus {
   class point;
   class mcmove;
+  class particle;
   class container;
 
 //  namespace Group {
 
     class group {
       public:
+        group();
+        string info();
+        string name;
         int beg;                  //!< index of first particle
         int end;                  //!< index of last particle
-        int len();                //!< number of particle in group.
+        int len() const;          //!< number of particles in group.
         vector<mcmove*> moves;    //!< pointers to move functions
 
         virtual double charge(const vector<particle>&) const;      //!< Calculate total charge
-        virtual point masscenter(const vector<particle>&) const;   //!< Calculate mass center
-        virtual point dipolemoment(const vector<particle>&) const; //!< Calculate dipole moment
+        virtual point masscenter(const container&) const;          //!< Calculate mass center
+        virtual point dipolemoment(const container&) const;        //!< Calculate dipole moment
 
         virtual void rotate(container&, const point&, double);     //!< Rotate around a vector
         virtual void translate(container&, const point&);          //!< Translate along a vector
         virtual void scale(container&, double);                    //!< Volume scaling
-        virtual void undo(container&);                             //!< Undo move operation
+        virtual void undo(space&);                                 //!< Undo move operation
+        virtual void accept(space&);
+
+        bool operator==(const group&) const;
+        group& operator+=(const group&);
+        const group operator+(const group&) const;
     };
 
-    class molecular : public group {
+    class molecular  {
       protected:
         point cm_trial;           //!< mass center vector for trial position
       public:
@@ -41,7 +50,7 @@ namespace Faunus {
         point cap1, cap2, patch;
     };
 
-//  }//namespace
+    //  }//namespace
 
 }//namespace
 

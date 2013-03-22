caa02d2404085f40ba6692b4a07d59b2b6018880
Mikael Lund <mlund@mac.com>
2013-03-22 16:23:07 +0100

    minor optimasation of dipole

--- include/faunus/multipole.h
+++ include/faunus/multipole.h
@@ -5,11 +5,14 @@ namespace Faunus {
 
   template<typename Tenergy>
     void getInducedDipoles(Tenergy &pot, Space &spc, const Eigen::MatrixXd &mu_per, double &limit) { 
-      using namespace Eigen;
       int size = spc.p.size();
-      double diLim = size*limit;   double diConv;
-      MatrixXd Field(size,3);      Field.setZero();
-      Point E(0,0,0);              Point mu_ind(0,0,0);    Point mu_err(0,0,0);
+      double diLim = size*limit;
+      double diConv;
+      Eigen::MatrixXd Field(size,3);
+      Field.setZero();
+      Point E(0,0,0);
+      Point mu_ind(0,0,0);
+      Point mu_err(0,0,0);
 
       do {  
         diConv = 0.0;
@@ -27,29 +30,37 @@ namespace Faunus {
       } while (diConv > diLim);
     }
 
-
   /**
    * @brief Returns dipole-dipole interaction which times the 
-   * @brief bjerrum length in (Å) gives the energy in k_BT.
    *
    * @param mu1 Unit dipole moment vector 1
    * @param mu2 Unit dipole moment vector 2
    * @param mu1xmu2 Product of dipole scalars
    * @param r Vector R_{1->2}
+   *
+   * T_{ab} = 3*r_a*r_b / |r|^5 , for a ne b
+   * T_{aa} = 3*r_a^2 / |r|^5 - 1 / |r|^3
+   * Summation of all T_{ab}*mu_a*mu_b
+   *
    */
   template<class Tvec>
     double mu2mu(const Tvec &mu1, const Tvec &mu2, double mu1xmu2, const Tvec &r) {
-      using namespace Eigen;
-      double R1 = 1.0/r.norm();        //   R^-1 , Å^-1
-      double R3 = R1*R1*R1;           //   R^-3 , Å^-3
-      double R5 = 3.0*R3*R1*R1;       // 3*R^-5 , Å^-5
-      Matrix3d T = R5*r*r.transpose() - R3*Matrix3d::Identity();  // T_{ab} = 3*r_a*r_b / |r|^5 , for a ne b , Å^-3
-      // T_{aa} = 3*r_a^2 / |r|^5 - 1 / |r|^3    , Å^-3
-      double W = -mu1.transpose()*T*mu2;                        // Summation of all T_{ab}*mu_a*mu_b       , Å^-3
-      return W*mu1xmu2;                             // Scaling with |mu_a|*|mu_b|              , Å^-1 e^ 2
+      double R2 = 1/r.squaredNorm();
+      double R1 = sqrt(R2);
+      double R3 = R1*R2;
+      double R5 = R3*R2;
+      Eigen::Matrix3d T = 3*R5*r*r.transpose() - R3*Matrix3d::Identity();
+      double W = -mu1.transpose()*T*mu2;
+      return W*mu1xmu2;
     }
 
   namespace Potential {
+
+    /**
+     * @brief Dipole-dipole interaction
+     *
+     * More info...
+     */
     class DipoleDipole : public PairPotentialBase {
       private:
         string _brief() { return "Dipole-dipole"; }
@@ -66,34 +77,36 @@ namespace Faunus {
             return _lB*mu2mu(a.mu,b.mu,a.muscalar*b.muscalar,r);
           }
 
-        Point field(const particle &p, const Point &r) const {
-          double R1 = 0.0;          double R2 = 0.0;   double R3 = 0.0;                                                        
-          Point E(0,0,0);                                                                         
-          R1 = 1.0/r.norm();
-          Point rn = r*R1;   R2 = R1*R1;   R3 = R2*R1;                                                       
-          E = E + p.charge*R2*rn;                                             // From charges                        
-          E = E + (3.0*p.mu.dot(rn)*rn - p.mu)*p.muscalar*R3;    // From dipoles                        
-          return E;                                                                                                        
-
-        }
+        /** @brief Dipole field at `r` due to dipole `p` */
+        template<class Tparticle>
+          Point field(const Tparticle &p, const Point &r) const {
+            double R2 = 1/r.squaredNorm();
+            double R1 = sqrt(R2);
+            double R3 = R1*R3;
+            Point rn = r*R1;
+            return (3.0*p.mu.dot(rn)*rn - p.mu)*p.muscalar*R3;
+          }
 
         string info(char w) { return _brief(); }
     };
 
+    /**
+     * @brief Dipole-dipole interaction w. spherical cutoff and reaction field
+     *
+     * More info...
+     */
     class DipoleDipoleRF : public DipoleDipole {
       private:
-        string _brief() { return "Reaction Field"; }
+        string _brief() { return "Dipole-dipole (RF)"; }
         double rc2,eps;
       public:
         DipoleDipoleRF(InputMap &in) : DipoleDipole(in) { 
-          rc2 = in.get<double>("dipdip_cutoff",pc::infty);
-          rc2 = rc2*rc2;
+          rc2 = pow(in.get<double>("dipdip_cutoff",pc::infty), 2);
           eps = in.get<double>("epsilon_rf",80.);
           eps = _lB*(2*(eps-1)/(eps+1))/pow(rc2,1.5);
         }
         template<class Tparticle>
           double operator()(const Tparticle &a, const Tparticle &b, const Point &r) const {
-            //cout << DipoleDipole::operator()(a,b,r) << " " << eps*a.mu.dot(b.mu)*a.muscalar*b.muscalar << "\n";
             if (r.squaredNorm() < rc2)
               return DipoleDipole::operator()(a,b,r) - eps*a.mu.dot(b.mu)*a.muscalar*b.muscalar;
             return 0.0;

fd26470ac994026758546154f9a67a02946f3eda
Mikael Lund <mlund@mac.com>
2013-06-26 12:56:20 +0200

    cigar documentation update

--- src/examples/cigars2fibrils.cpp
+++ src/examples/cigars2fibrils.cpp
@@ -64,40 +64,48 @@ int main() {
 }
 
 /**
- @page example_cigars2fibrils Example: Fibril formation from patchy spherocylinders
+ @page example_cigars2fibrils Example: Fibrils from patchy spherocylinders
  
- In this example we simulate self-assembly of patchy spherocylinders (cigars) in the
- NVT ensemble. Spherocylinder is a cylinder with hemispherical caps at both ends, thus 
- providing a smooth rod-like particle with a variable aspect ratio. An attractive angular 
- was added to the side of a spherocylinder, creating a patchy spherocylinder. This patch 
- can either run along the whole axis including the ends (patchtype 1) as in this example,
- or it can be limited to the cylindrical part (patchtype 2). The attractive interaction 
- is then determined from overlapping segments of the two spherocylinders withn their patches.
- We use a  WeeksChandlerAnderson potential for repulsive part of the particle combined 
- with a cosine squared potential for attractive interactions of patches. This is an 
- implicit solvent model, where interactions are effective interactions and including solvent
- effects, hydrogen bonds, etc.  In this short example we use a angular size of a patch 
- of 90 degrees. This results in self-assembly of fibrils with 3 spherocylinders in 
- cross-section (see figure below). Note that the timescale of this short example does not 
- allow to reach equilibrium conditions. For more details see input files in `src/examples/cigars2fibrils.run`.
+ In this example we simulate self-assembly of patchy spherocylinders (cigars)
+ in the NVT ensemble.
+ Spherocylinder is a cylinder with hemispherical caps at both ends, thus 
+ providing a smooth rod-like particle with a variable aspect ratio.
+ An attractive angular was added to the side of a spherocylinder,
+ creating a patchy spherocylinder. This patch  can either run along the whole
+ axis including the ends (patchtype 1) as in this example, or it can be
+ limited to the cylindrical part (patchtype 2).
+ The attractive interaction is then determined from overlapping segments of
+ the two spherocylinders withn their patches.
+ We use a Weeks-Chandler-Anderson potential for the repulsive part of the
+ particle combined  with a cosine squared potential for attractive
+ interactions of patches.
+ This is an implicit solvent model, where interactions are effective
+ interactions and including solvent effects, hydrogen bonds, etc.
  
- The `cigars2fibrils.cpp` program can be used to simulate self-assembly of patchy 
- spherocylinders with various aspect rations and patches at various temperatures.
+ In this short example we use a angular size of a patch 
+ of 90 degrees. This results in self-assembly of fibrils with three
+ spherocylinders in cross-section (see figure below). Note that the
+ timescale of this short example does not  allow to reach equilibrium
+ conditions.
+ For more details see input files in `src/examples/cigars2fibrils.run`.
  
+ The `cigars2fibrils.cpp` program can be used to simulate self-assembly of
+ patchy spherocylinders with various aspect rations and patches at various
+ temperatures.
  We have the following MC moves:
  - cigars translation
  - cigars rotations
  
- After simulation one can run visualisation script cigars-mov2pdb.py to convert the output 
- trajectory to input files, which can be visualised by VMD (Visual Molecular Dynamics) 
- program freely available at `http://www.ks.uiuc.edu/Research/vmd/`. Note that it is not 
- straight forward to visualise angular wedge on the spherocylinders in VMD. To guied an eye 
- for direction and size of patch we display additional spherocylinder that represents the patch.
+ After simulation one can run a visualisation script `cigars-mov2pdb.py` to
+ convert the output trajectory to input files, which can be visualised by
+ VMD (Visual Molecular Dynamics) program freely available at
+ `http://www.ks.uiuc.edu/Research/vmd/`. Note that it is not 
+ straight forward to visualise the angular wedge on the spherocylinders
+ in VMD. To guied an eye for direction and size of patch we display
+ additional spherocylinder that represents the patch.
  
- ![Snapshot of short fibrils formed in cigars2fibrils example with patchye spherocylinders.
- Blue color represents the body of spherocylinders while red spherocylinders is used for 
- visualisation of patch direction.](cigars2fibrils.png)
- 
+![Snapshot of short fibrils formed in cigars2fibrils example with patchye spherocylinders. Blue color represents the body of spherocylinders while red spherocylinders is used for visualisation of patch direction.](cigars2fibrils.png)
+
  cigars2fibrils.cpp
  ========
  \includelineno examples/cigars2fibrils.cpp

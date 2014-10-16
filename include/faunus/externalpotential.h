146d785e43d3bea451a8f681f400cddf84ae131f
João Henriques <joaoh@frida.teokem.lu.se>
2014-10-16 11:58:31 +0200

    little detail with indentation

--- include/faunus/externalpotential.h
+++ include/faunus/externalpotential.h
@@ -391,13 +391,13 @@ namespace Faunus {
           T _depth;
           T _threshold;
           std::string _info();
-	enum InteractionType {SQWL, LJ, R6, R3};    //
-          InteractionType _type;              // faster than evaluating strings
+          enum InteractionType {SQWL, LJ, R6, R3}; //
+          InteractionType _type;                   // faster than evaluating strings
         public:
           StickyWall(InputMap&);
-          void setSurfPositionZ(T*);          // sets position of surface
+          void setSurfPositionZ(T*);               // sets position of surface
           template<typename Tparticle>
-            T operator()(const Tparticle &p); // returns energy
+            T operator()(const Tparticle &p);      // returns energy
       };
 
     template<class T>

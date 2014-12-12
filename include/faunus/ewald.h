02c60ba6e89339d31d32dc818a58581643f3ee5e
Mikael Lund <mlund@mac.com>
2014-12-12 18:17:00 +0100

    Ewald update

--- include/faunus/ewald.h
+++ include/faunus/ewald.h
@@ -10,15 +10,17 @@ namespace Faunus {
 
     /**
      * @brief Pair potential for real-space part of Ewald
+     *
+     * Explain, insert LaTeX
      */
     template<bool useIonIon=true, bool useIonDipole=false, bool useDipoleDipole=false> 
       struct EwaldReal : public Potential::Coulomb {
 
-        typedef Potential::Coulomb base;
+        typedef Potential::Coulomb Tbase;
         double alpha, rc2i;
 
-        EwaldReal(InputMap &in, string pfx="coulomb") : base(in,pfx), alpha(0), rc2i(0) {
-          base::name="Ewald Real";
+        EwaldReal(InputMap &in, string pfx="coulomb") : Tbase(in,pfx), alpha(0), rc2i(0) {
+          Tbase::name="Ewald Real";
         }
 
         template<class Tparticle>
@@ -50,12 +52,14 @@ namespace Faunus {
               E += -(t5 + t3)*b.muscalar*a.muscalar;
             }
 #endif
-            return base::bjerrumLength() * E; 
+            return Tbase::bjerrumLength() * E; 
           }
       };
   }//namespace
 
   namespace Energy {
+
+    using namespace Faunus::Potential;
 
     /**
      * @brief Ewald summation for electrostatic interactions
@@ -68,25 +72,40 @@ namespace Faunus {
      * `ewald_precision` |  Precision ...
      *
      * @todo arbitrary box dimensions; documentation; optimization; automatic alpha calc.
-     *      prettify _info() output (see other energy classes)
+     *      prettify _info() output (see other energy classes); seach for "todo" items below
      */
-    template<class Tspace, class Tpairpot, bool useIonIon=true, bool useIonDipole=false, bool useDipoleDipole=false>
-      class NonbondedEwald : public NonbondedVector<Tspace,
-      Potential::CombinedPairPotential<Potential::EwaldReal<useIonIon,useIonDipole,useDipoleDipole>, Tpairpot> > {
+    template<
+      class Tspace, \
+      class Tpairpot, \
+      bool useIonIon=true, bool useIonDipole=false, bool useDipoleDipole=false, \
+      class Tbase=NonbondedVector<Tspace, \
+      CombinedPairPotential<EwaldReal<useIonIon,useIonDipole,useDipoleDipole>, Tpairpot>>>
+
+      class NonbondedEwald : public Tbase {
         private:
-          typedef NonbondedVector<Tspace,
-                  Potential::CombinedPairPotential<
-                    Potential::EwaldReal<useIonIon,useIonDipole,useDipoleDipole>, Tpairpot> > base;
+          using Tbase::spc;
+          typedef typename Tbase::Tparticle Tparticle;
+          typedef typename Tbase::Tpvec Tpvec;
 
-          using base::spc;
-          typedef typename base::Tparticle Tparticle;
-          typedef typename base::Tpvec Tpvec;
+          int maxK, maxKz, k2max;
+          double alpha, alpha2, V, eps_surf, eps_r, Rc, Rc2, rc2i,
+                 constant, L, lB, const_inf,
+                 check_k2_zero, ewapre, lamsep, lamell;
 
+          bool user_alpha, user_maxK;
+
+          /// @todo ARE THESE AVERAGES NEEDED ?
+          Average<double> selfEA, surfEA, realEA, reciEA;
+
+          Eigen::MatrixXd kVectors;
+          Eigen::VectorXd k2s, Aks;  // explain...
+
+          /// @todo Why do we not reach inner loop??
           void updateKVectors() {
             kVectors.setZero();
             k2s.setZero();
             Aks.setZero();
-            wavefunctions = 0;
+            int wavefunctions = 0;
             for (int kx = 0; kx <= maxK; kx++) 
               for (int ky = -maxK; ky <= maxK; ky++) 
                 for (int kz = -maxKz; kz <= maxKz; kz++) {
@@ -100,79 +119,78 @@ namespace Faunus {
                   k2s[wavefunctions] = k2;
                   Aks[wavefunctions] = exp(-k2/(4*alpha2))/k2;
                   wavefunctions++;
+                  assert("Why do we never reach here!!");
                 }
+            //assert( wavefunction == kVectors.cols());
+            cout << wavefunctions << " " << kVectors.cols() << endl;
           }
 
           template<class Tpvec, class Tgroup>
-            double getSelfEnergy(const Tpvec &p, const Tgroup &g) {
-              double Eq = 0.0;
-              double Emu = 0.0;
-              for (auto n : g) {
+            double getSelfEnergy(const Tpvec &p, const Tgroup &g) const {
+              double Eq=0, Emu=0;
+              for (auto i : g) {
                 if (useIonIon || useIonDipole)
-                  Eq = Eq + p[n].charge*p[n].charge;
+                  Eq += p[i].charge * p[i].charge;
 #ifdef DIPOLEPARTICLE
                 if (useIonDipole || useDipoleDipole)
-                  Emu = Emu + p[n].mu.dot(p[n].mu)*p[n].muscalar*p[n].muscalar;
+                  Emu += p[i].mu.dot(p[i].mu) * p[i].muscalar * p[i].muscalar;
 #endif
               }
-              return -(alpha/sqrt(pc::pi))*( Eq + (2*alpha2/3)*Emu );
+              return -alpha/sqrt(pc::pi) * ( Eq + 2*alpha2/3*Emu );
             }
 
           template<class Tpvec, class Tgroup>
-            double getSurfEnergy(const Tpvec &p, const Tgroup &g) {
+            double getSurfEnergy(const Tpvec &p, const Tgroup &g) const {
               Point mus(0,0,0), qrs(0,0,0);
-              for (auto n : g) {
+              for (auto i : g) {
                 if (useIonIon || useIonDipole)
-                  qrs = qrs + p[n].charge*p[n];
+                  qrs = qrs + p[i].charge*p[i];
 #ifdef DIPOLEPARTICLE
                 if (useIonDipole || useDipoleDipole)
-                  mus = mus + p[n].mu*p[n].muscalar;
+                  mus = mus + p[i].mu*p[i].muscalar;
 #endif
               }
-              return const_inf*(2*pc::pi/(( 2*eps_surf + 1)*V))*(qrs.dot(qrs) + 2*qrs.dot(mus) +  mus.dot(mus) );
+              return const_inf * 2*pc::pi/(( 2*eps_surf + 1)*V)
+                * ( qrs.dot(qrs) + 2*qrs.dot(mus) +  mus.dot(mus) );
             }
 
           /**
            * @brief ... 
+           * @todo reduce number of calls to cos/sin
            */
           template<class Tpvec, class Tgroup>
-            double getQ2(const Tpvec &p, const Tgroup &g, const Point &kv) {
-              std::complex<double> Q2(0, 0);
+            double getQ2(const Tpvec &p, const Tgroup &g, const Point &kv) const {
+              complex<double> Q2(0,0);
               for (auto i : g) {
-                double dotP = kv.dot( p[i] );
-                if (useIonIon || useIonDipole) {
-                  std::complex<double> temp0(cos(dotP),sin(dotP));
-                  Q2 = Q2 + p[i].charge*temp0;
-                }
+                double dot = kv.dot(p[i]),
+                       cosdot=cos(dot), sindot=sin(dot);
+                if (useIonIon || useIonDipole)
+                  Q2 += p[i].charge * complex<double>(cosdot,sindot);
 #ifdef DIPOLEPARTICLE
-                if (useIonDipole || useDipoleDipole) {
-                  std::complex<double> temp1(-sin(dotP),cos(dotP));
-                  Q2 = Q2 + kv.dot(p[i].mu)*p[i].muscalar*temp1;
-                }
+                if (useIonDipole || useDipoleDipole)
+                  Q2 += Q2 + kv.dot(p[i].mu) * p[i].muscalar * complex<double>(-sindot,cosdot);
 #endif
               }
               double dQ2 = std::abs(Q2);
               return dQ2*dQ2;
             }
 
+          /// @todo floating point comparison `alpha2==0` bad 
           template<class Tpvec, class Tgroup>
-            double getReciEnergy(const Tpvec &p, const Tgroup &g) {
-              double E = 0.0;
-              if(alpha2 == 0)
+            double getReciEnergy(const Tpvec &p, const Tgroup &g) const {
+              double E=0;
+              if (alpha2==0)
                 return E;
-              double Q2;
-              for(int i = 0; i < kVectorsLength; i++) {
-                Q2 = getQ2(p,g,kVectors.col(i));
-                E += Aks[i]*Q2;
-              }
+              for (int i=0; i<kVectors.cols(); ++i)
+                E += Aks[i] * getQ2(p,g,kVectors.col(i));
               return (2*pc::pi/V)*E;
             }
 
-
+          /// @todo kVectorsLength = kVectors.cols()
           void calcMaxK(double alpha2_in, double L_in) {
             maxK  = std::ceil(1.0 + alpha2_in*Rc*L_in/pc::pi);
             maxKz = std::ceil(1.0 + alpha2_in*Rc*lamsep/pc::pi);
-            kVectorsLength = (maxK + 1)*(2*maxK + 1)*(2*maxKz + 1) - 1;
+            int kVectorsLength = (maxK + 1)*(2*maxK + 1)*(2*maxKz + 1) - 1;
             kVectors.resize(3, kVectorsLength); 
             k2s.resize(kVectorsLength);
             Aks.resize(kVectorsLength);
@@ -182,6 +200,7 @@ namespace Faunus {
            * @brief ....
            * @param rcut2 Real space cut-off
            * @param ....
+           * @todo Use `std::cerr` instead of `cout`
            */
           void calcAlpha(double inewaldpression, double rcut2) {
             double ewaldpres = inewaldpression;
@@ -246,11 +265,12 @@ namespace Faunus {
             constant = 2*alpha/sqrt(pc::pi);
           }
 
+          /// @todo prettify to match normal faunus output
           string _info() {
             std::ostringstream o;
-            o << base::_info()
+            o << Tbase::_info()
               << "maxK                    = " << maxK << endl
-              << "wavefunctions           = " << wavefunctions << endl
+              << "wavefunctions           = " << kVectors.cols() << endl
               << "alpha                   = " << alpha << endl
               << "R_c                     = " << Rc << endl
               << "<Self energy>           = " << selfEA.avg() << endl
@@ -260,23 +280,13 @@ namespace Faunus {
             return o.str();
           }
 
-          int N, maxK, maxKz, kVectorsLength, k2max, wavefunctions;
-          double alpha, alpha2, V, eps_surf, eps_r, Rc, Rc2, rc2i,
-                 constant, L, lB, T, const_inf,
-                 check_k2_zero, ewapre, lamsep, lamell;
-
-          bool user_alpha, user_maxK;
-
-          Average<double> selfEA, surfEA, realEA, reciEA;
-          Eigen::MatrixXd kVectors;
-          Eigen::VectorXd k2s, Aks;  // explain...
-
         public:
-          NonbondedEwald(InputMap &in, string pfx="ewald") : base(in) {
-            base::name += " (Ewald)";
+          /// @todo see note below in CAPS. Lamell needed??
+          NonbondedEwald(InputMap &in, string pfx="ewald") : Tbase(in) {
+            Tbase::name += " (Ewald)";
             ewapre = in.get<double>(pfx+"_precision",0.01);
 
-            lB = base::pairpot.first.bjerrumLength();
+            lB = Tbase::pairpot.first.bjerrumLength();
             L = typename Tspace::GeometryType(in).len.norm(); //in.get<double>("cuboid_len",pc::infty);
             lamell = in.get<double>(pfx+"_lamell",0.0);
             lamsep = lamell + L;
@@ -285,7 +295,7 @@ namespace Faunus {
               Rc=L/2.;
             Rc2 = Rc*Rc;
             rc2i = 1.0/(Rc*Rc);
-            base::pairpot.first.rc2i=rc2i;
+            Tbase::pairpot.first.rc2i=rc2i;
             eps_surf = in.get<double>(pfx+"_eps_surf",pc::infty);
             const_inf = 1;
             if(eps_surf > 1e10)  // \epsilon_{Surface} = \infty
@@ -299,12 +309,13 @@ namespace Faunus {
               constant = 2*alpha/sqrt(pc::pi);
             }
 
+            // THIS SEEMS TO BE THE SAME AS `calcMaxK` ??
             user_maxK = false;
             if (in.get<double>(pfx+"_maxK", -1.0  ) != -1.0 ) {
               user_maxK = true;
               maxK = in.get<double>(pfx+"_maxK", -1.0  );
               maxKz = maxK;
-              kVectorsLength = (maxK + 1)*(2*maxK + 1)*(2*maxKz + 1) - 1;
+              int kVectorsLength = (maxK + 1)*(2*maxK + 1)*(2*maxKz + 1) - 1;
               kVectors.resize(3, kVectorsLength); 
               k2s.resize(kVectorsLength);
               Aks.resize(kVectorsLength);
@@ -323,29 +334,25 @@ namespace Faunus {
           /**
            * @brief Set space and update k-vectors and alpha
            */
-          void setSpace(Tspace &s) {
-            base::setSpace(s);
+          void setSpace(Tspace &s) FOVERRIDE {
+            Tbase::setSpace(s);
             V = spc->geo.getVolume();
             L = std::cbrt(V);
-            N = 0;
-            for (auto &i : spc->p)
-              if (fabs(i.charge)>1e-6)
-                N++;
-            if(!user_alpha)
+            if (!user_alpha)
               calcAlpha(ewapre,Rc2);
             alpha2 = alpha*alpha;
-            if(!user_maxK)
+            if (!user_maxK)
               calcMaxK(alpha2,L);
             check_k2_zero = 0.1*L*L/(4*pc::pi*pc::pi);
 
             k2max = std::ceil(4.0*pow(((pc::pi/L) + alpha2*Rc),2));  // From Martin's program
             int k2maxtemp = std::ceil(4*pow(((pc::pi/lamsep) + alpha2*Rc),2));
-            if(k2maxtemp > k2max) 
+            if (k2maxtemp > k2max) 
               k2max = std::ceil(k2maxtemp);
 
             updateKVectors();
 
-            base::pairpot.first.alpha = alpha;
+            Tbase::pairpot.first.alpha = alpha;
           }
       };
 

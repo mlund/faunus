e5dcdb492c8b9acb7b19bb2b6f1b802d9d6c11ad
Mikael Lund <mlund@mac.com>
2014-12-17 15:13:18 +0100

    Stripped slump.h for anything but mersenne twister

--- include/faunus/slump.h
+++ include/faunus/slump.h
@@ -7,88 +7,32 @@
 
 namespace Faunus {
 
-  /*@
-   * @brief Base class for random number generation
-   * @todo To be removed -- C++11 random twister should be default.
-   *
-   * Derived classes need only provide `_randone()` and `seed()` function.
-   */
-  class RandomBase {
-    private:
-      virtual double _randone()=0;  //!< Random number in range `[0,1)`
-    public:
-      std::string name;
-      virtual ~RandomBase() {};
-  
-      /** @brief Random number in range `[-0.5,0.5)` */
-      inline double randHalf() { return _randone() - 0.5; }
-
-      /** @brief Random number in range `[0,max unsigned int)` */
-      inline unsigned int rand() {
-        static const double max=std::numeric_limits<unsigned int>::max()-1;
-        return _randone() * max;
-      }
-
-      /** @brief Random number in range [0,1) */
-      inline double operator()() {
-        double x=_randone();
-        assert(x>=0 && x<1 && "Random number out of range!");
-        return x;
-      }
-  };
-
   /**
-   * @brief Ran2 Random Number Gererator
-   * @author Bjorn Persson
-   * @date Lund, 2008
-   * @note A class for ran2 from 'Numerical Recipies'.
-   * @warning Not thread safe!
-   * @todo To be removed -- C++11 random twister should be default.
-   */
-  class RandomRan2: public RandomBase {
-    private:
-      static const int IM1=2147483563, IM2=2147483399;
-      static const int IA1=40014, IA2=40692, IQ1=53668, IQ2=52774;
-      static const int IR1=12211, IR2=3791, NTAB=32, IMM1=IM1-1;
-      static const int NDIV=1+IMM1/NTAB;
-      static const double EPS;//=3.0e-16;
-      double AM,RNMX;
-      int idum, idum2, iy;
-      int iv[32]; 
-      int j,k;
-      double temp;
-      double _randone();
-    public:
-      RandomRan2();
-      void seed(int=-7);
-  };
-
-  /**
-   * @brief Mersenne Twister Random number functions (C++11)
-   *
-   * This uses the C++11 Mersenne Twister for random number generation and while
-   * slower than ran2, MT generally provides better randomness.
-   *
+   * @brief Mersenne Twister Random number generator
    * @date Lund, 2010
    */
   template<typename T=double, typename Tengine=std::mt19937> 
-    class RandomTwister : public RandomBase {
+    class RandomTwister {
+
       private:
-        Tengine eng; //pseudo random number engine
+
+        Tengine eng; //random number engine
+
         std::uniform_real_distribution<T> dist;
+
         T _randone() {
           T x;
 #pragma omp critical
           x=dist(eng);
           return x;
         }
-      public:
-        /** @brief Constructor -- default deterministic seed is used */
-        RandomTwister() : dist(0,1) {
-          name="Mersenne-Twister";
-        }
 
-        /** @brief Integer in range [min:max] */
+      public:
+
+        /// @brief Constructor -- default deterministic seed is used
+        RandomTwister() : dist(0,1) {}
+
+        /// @brief Integer in range [min:max]
         int range(int min, int max) {
           std::uniform_int_distribution<int> d(min,max);
           return d(eng);
@@ -108,19 +52,28 @@ namespace Faunus {
 #pragma omp critical
             eng.seed(s);
           }
+
+        /// @brief Random number in range `[-0.5,0.5)`
+        T randHalf() { return _randone() - 0.5; }
+
+        /// @brief Random number in range [0,1)
+        T operator()() { return _randone(); }
+
+        /**
+         * @brief Random number in range `[0,max unsigned int)`
+         * @todo To be removed
+         */
+        unsigned int rand() {
+          return range(0, std::numeric_limits<unsigned int>::max()-1);
+        }
     };
 
-#define MERSENNETWISTER
-
-#if defined(MERSENNETWISTER)
   typedef Faunus::RandomTwister<double,std::mt19937> slump;
-#else
-  typedef Faunus::RandomRan2 slump;
-#endif
+
   extern slump slp_global;
 
   /**
-   * @brief Get random element from STL container
+   * @brief Get random element from iterable container (vector, map, group etc.)
    * @return Iterator to random element
    */
   template<class Titer>
--- src/slump.cpp
+++ src/slump.cpp
@@ -1,59 +1,6 @@
 #include <faunus/slump.h>
 
 namespace Faunus {
-  const double RandomRan2::EPS=3.0e-16;
-
-  RandomRan2::RandomRan2() {
-    name="Numerical Recipes 'ran2'";
-    AM=1.0/2147483563.0;
-    RNMX=1.0-3.0e-16;
-    iy=0;
-    idum2=123456789;
-    seed(-13);
-  }
-
-  double RandomRan2::_randone()  {
-    #pragma omp critical
-    {
-      seed(idum);
-      k=idum/IQ1;
-      idum=IA1*(idum-k*IQ1)-k*IR1;
-      if (idum<0)
-        idum+=IM1;
-      k=idum2/IQ2;
-      idum2=IA2*(idum2-k*IQ2)-k*IR2;
-      if (idum2<0)
-        idum2+=IM2;
-      j=iy/NDIV;
-      iy=iv[j]-idum2;
-      iv[j]=idum;
-      if (iy<1)
-        iy+=IMM1;
-    }
-    double temp;
-    if((temp=AM*iy)>RNMX)
-      return RNMX;
-    else
-      return temp;
-  }
-
-  void RandomRan2::seed(int s) {
-    idum=s;    
-    if(idum<=0) {
-      idum=(idum==0 ? 1: -idum);
-      idum2=idum;
-      for (j=NTAB+7; j>=0; j--) {
-        k=idum/IQ1;
-        idum=IA1*(idum-k*IQ1)-k*IR1;
-        if (idum<0) 
-          idum+=IM1;
-        if (j<NTAB)
-          iv[j]=idum;
-      }
-      iy=iv[0];
-    }
-  }
-
   slump slp_global;
 }//namespace
 

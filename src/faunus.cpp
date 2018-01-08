d69a85feb750eb1d5a8c6ff4f6cbcc4b41c3ce47
Mikael Lund <mlund@mac.com>
2018-01-08 23:47:59 +0000

    update

--- src/faunus.cpp
+++ src/faunus.cpp
@@ -13,36 +13,47 @@ typedef Geometry::Cuboid Tgeometry;
 typedef Particle<Charge> Tparticle;
 
 static const char USAGE[] =
-R"(Faunus - A Framework for Molecular Simulation.
+R"(Hoth - the Monte Carlo code you're looking for!
 
     http://github.com/mlund/faunus
 
     Usage:
-      faunus [-q] [--rerun=TRAJ] [--state=FILE] [-]
+      faunus [-q] [--nobar] [--input=<file>] [--output=<file>]
       faunus (-h | --help)
       faunus --version
 
     Options:
-      --rerun=TRAJ               Rerun w. trajectory file (.xtc).
-      -s FILE, --state=FILE      Initialize using state file.
-      -q, --quiet                Less verbose output.
+      
+      -i <file> --input <file>   Input file [default: /dev/stdin].
+      -o <file> --output <file>  Output file [default: out.json].
+      --nobar                    No progress bar.
+      -q --quiet                 Less verbose output.
       -h --help                  Show this screen.
       --version                  Show version.
 )";
 
 int main( int argc, char **argv )
 {
+    Faunus::MPI::MPIController mpi; // OK also if MPI is unavailable
+
     try {
         auto args = docopt::docopt( USAGE,
                 { argv + 1, argv + argc }, true, "Faunus 2.0.0");
 
-        if ( args["--quiet"].asBool() )
-            cout.setstate( std::ios_base::failbit ); // hold kæft!
+        bool showProgress = !args["--nobar"].asBool();
 
-        Faunus::MPI::MPIController mpi; // allowed even if MPI is unavailable
+        bool quiet = args["--quiet"].asBool();
+        if (quiet) {
+            cout.setstate( std::ios_base::failbit ); // hold kæft!
+            showProgress = false;
+        }
 
         json j;
-        std::cin >> j;
+        auto inputFile = args["--input"].asString();
+        if (inputFile=="/dev/stdin")
+            std::cin >> j;
+        else
+            j = openjson(mpi.prefix + inputFile);
 
         pc::temperature = j.at("temperature").get<double>() * 1.0_K;
         MCSimulation<Tgeometry,Tparticle> sim(j);
@@ -56,7 +67,7 @@ int main( int argc, char **argv )
         for (int i=0; i<macro; i++) {
             for (int j=0; j<micro; j++) {
 
-                if (mpi.isMaster()) {
+                if (showProgress && mpi.isMaster()) {
                     ++progressBar;
                     if (j % 10 == 0)
                         progressBar.display();
@@ -68,11 +79,12 @@ int main( int argc, char **argv )
         }
         progressBar.done();
 
-        mpi.cout << "relative drift = " << sim.drift() << endl;
+        if (!quiet) {
+            mpi.cout << "relative drift = " << sim.drift() << endl;
+            mpi.cout << json(mpi) << endl;
+        }
 
-        mpi.cout << json(mpi) << endl;
-
-        std::ofstream f(mpi.prefix + "out.json");
+        std::ofstream f(mpi.prefix + args["--output"].asString());
         if (f) {
             json j = sim;
             j["analysis"] = analysis;

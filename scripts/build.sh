6b431ccb7775088bb525c18332941ed9cd7df270
Mikael Lund <mlund@mac.com>
2018-01-31 23:28:41 +0100

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 export CC=clang
 export CXX=clang++
-cmake -DENABLE_MPI=on -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX
+CXX=clang++ CC=clang cmake -DENABLE_MPI=on -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX
 make faunus
 make pyfaunus
 make tests

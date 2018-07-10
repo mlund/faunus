8ead60d700956d238d201227e87e93251eb0510e
gitesei <giu.tesei@gmail.com>
2018-07-10 18:55:05 +0200

    Update

--- src/penalty.h
+++ src/penalty.h
@@ -170,11 +170,16 @@ namespace Faunus {
                     cout << "Molecule Name: " << name << endl;
                     f = [&spc, dir=dir, i=index]() {
                         auto &cm = spc.groups[i].cm;
+                        Point ete = spc.geo.vdist(spc.groups[i].begin()->pos,(spc.groups[i].begin()+2)->pos)
                         auto S = Geometry::gyration(spc.groups[i].begin(), spc.groups[i].end(), spc.geo.boundaryFunc, cm);
                         Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> esf(S);
                         Point vec = esf.eigenvectors().col(2);
-                        double cosine = vec.dot(dir);
+                        double cosine1 = vec.dot(dir);
+                        double angle1 = acos(abs(cosine1)) * 180. / pc::pi;
+                        cout << "angle from S " << angle1 << endl;
+                        double cosine = ete.dot(dir);
                         double angle = acos(abs(cosine)) * 180. / pc::pi;
+                        cout << "angle from ete " << angle << endl;
                         return angle; 
                     };
                 }

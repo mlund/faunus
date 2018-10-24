436b5b91ab123ee1f18fca2834867d52f71ce337
Mikael Lund <mlund@mac.com>
2018-10-24 09:41:16 +0200

    disable faunus variants for now

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,8 @@ CC=clang
 CXX=clang++ CC=clang cmake -DENABLE_MPI=on -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX
 alias make="make -s"
 make faunus
-make faunus_nopbc
-make faunus_sphere
+#make faunus_nopbc
+#make faunus_sphere
 #make manual
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX # needed to build pyfaunus
 make pyfaunus

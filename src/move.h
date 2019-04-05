775ddac034e86d7af9dfa2f01582f9a41e936ef0
work <bjorn.stenqvist@teokem.lu.se>
2019-04-05 10:45:38 +0200

    Added ExcludedVolume as an external potential. This function removes the availability of a specified region of the simulation cell. Also added user specific probability to include groups in a cluster in cluster-move.

--- src/move.h
+++ src/move.h
@@ -950,7 +950,7 @@ namespace Faunus {
                     typedef typename Tspace::Tgroup Tgroup;
                     Tspace& spc;
                     Average<double> msqd, msqd_angle, N;
-                    double dptrans=0, dprot=0, angle=0, _bias=0;
+                    double dptrans=0, dprot=0, angle=0, _bias=0, probability=1.0;
                     size_t bias_rejected=0;
                     bool rotate; // true if cluster should be rotated
                     Point dir={1,1,1}, dp;
@@ -963,14 +963,14 @@ namespace Faunus {
 
                     virtual double clusterProbability(const Tgroup &g1, const Tgroup &g2) const {
                         if ( spc.geo.sqdist(g1.cm, g2.cm) <= thresholdsq(g1.id,g2.id) )
-                            return 1.0;
+                            return probability;
                         return 0.0;
                     }
 
                     void _to_json(json &j) const override {
                         using namespace u8;
                         j = {
-                            {"dir", dir}, {"dp", dptrans}, {"dprot", dprot},
+                            {"dir", dir}, {"dp", dptrans}, {"dprot", dprot}, {"probability", probability},
                             {rootof + bracket("r" + squared), std::sqrt(msqd.avg())},
                             {rootof + bracket(theta + squared) + "/" + degrees, std::sqrt(msqd_angle.avg()) / 1.0_deg},
                             {bracket("N"), N.avg()},
@@ -999,10 +999,11 @@ namespace Faunus {
                     }
 
                     void _from_json(const json &j) override {
-                        assertKeys(j, {"dp", "dprot", "dir", "threshold", "molecules", "repeat", "satellites"});
+                        assertKeys(j, {"dp", "dprot", "dir", "threshold", "molecules", "repeat", "satellites", "probability"});
                         dptrans = j.at("dp");
                         dir = j.value("dir", Point(1,1,1));
                         dprot = j.at("dprot");
+                        probability = j.value("probability",1.0);
                         names = j.at("molecules").get<decltype(names)>(); // molecule names
                         ids = names2ids(molecules<Tpvec>, names);     // names --> molids
                         index.clear();

7aa198bcae1b071267e966286f97215561ad13fc
Richard Chudoba <richard.chudoba@teokem.lu.se>
2019-04-26 23:34:30 +0200

    functionparser code formatting

--- src/functionparser.h
+++ src/functionparser.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <string>
 #include <exprtk.hpp> // https://github.com/ArashPartow/exprtk
 #include <nlohmann/json.hpp>
@@ -8,28 +9,29 @@
  * with a shared pointer, allowing `ExprFunction`
  * to be directly assigned to `std::function`.
  */
-template <typename T=double> class ExprFunction {
-    private:
-        std::shared_ptr<exprtk::parser<T>> parser;
-        exprtk::expression<T> expression;
-        exprtk::symbol_table<T> symbols;
-        typedef std::vector<std::pair<std::string,T*>> Tvarvec;
-        typedef std::vector<std::pair<std::string,T>> Tconstvec;
-    public:
-        void set(const std::string &exprstr, const Tvarvec &vars={}, const Tconstvec &consts={});
-        void set(const nlohmann::json &j, const Tvarvec &vars={});
-        T operator()() const;
+template<typename T=double>
+class ExprFunction {
+    std::shared_ptr<exprtk::parser<T>> parser;
+    exprtk::expression <T> expression;
+    exprtk::symbol_table <T> symbols;
+    typedef std::vector<std::pair<std::string, T*>> Tvarvec;
+    typedef std::vector<std::pair<std::string, T>> Tconstvec;
+
+  public:
+    void set(const std::string &exprstr, const Tvarvec &vars = {}, const Tconstvec &consts = {});
+    void set(const nlohmann::json &j, const Tvarvec &vars = {});
+    T operator()() const;
 };
 
 
 #ifdef DOCTEST_LIBRARY_INCLUDED
 TEST_CASE("[Faunus] ExprFunction") {
-    double x=0, y=0;
+    double x = 0, y = 0;
     ExprFunction<double> expr;
-    nlohmann::json j = R"( { "function":"x*x+kappa", "constants":{ "kappa":0.4, "f":2 } })"_json;
-    expr.set(j, {{"x",&x}, {"y",&y}} );
+    nlohmann::json j = R"({ "function": "x*x+kappa", "constants": {"kappa": 0.4, "f": 2} })"_json;
+    expr.set(j, {{"x", &x}, {"y", &y}});
     std::function<double()> f = expr;
-    x=4;
-    CHECK(f() == doctest::Approx(4*4+0.4));
+    x = 4;
+    CHECK( f() == doctest::Approx(4*4+0.4) );
 }
 #endif

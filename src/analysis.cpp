e867a32c89d4a8665f20e038c4160658ba51b87b
Mikael Lund <mlund@mac.com>
2020-02-02 21:19:57 +0100

    Fix formatting error for reactioncoordinate and qrtraj

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -343,7 +343,7 @@ QRtraj::QRtraj(const json &j, Space &spc) {
     write_to_file = [&groups = spc.groups, &f = f]() {
         for (auto &g : groups) {
             for (auto it = g.begin(); it != g.trueend(); ++it) { // loop over *all* particles
-                f << (it < g.end()) ? fmt::format("{.6f} {.6f} ", it->charge, atoms[it->id].sigma * 0.5) : "0 0 ";
+                f << (it < g.end()) ? fmt::format("{:.6f} {:.6f} ", it->charge, atoms[it->id].sigma * 0.5) : "0 0 ";
             }
         }
         f << "\n";               // newline for every frame
@@ -448,7 +448,7 @@ void FileReactionCoordinate::_sample() {
     if (file) {
         double val = (*rc)();
         avg += val;
-        file << fmt::format("{} {.6f} {.6f}\n", cnt * steps, val, avg.avg());
+        file << fmt::format("{} {:.6f} {:.6f}\n", cnt * steps, val, avg.avg());
     }
 }
 

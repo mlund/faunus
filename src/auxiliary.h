fdd30e3f3a36c2f58552384e477b36eeba44b9a8
Mikael Lund <mlund@mac.com>
2020-07-10 19:06:15 +0200

    Maka table lookup optional

--- src/auxiliary.h
+++ src/auxiliary.h
@@ -326,16 +326,20 @@ namespace Faunus
      *       if a range of degrees are needed
      * @tparam T floating point type (double, float, ...)
      * @tparam n Maximum order to evaluate
+     * @tparam use_table Use lookup table for 1+1/i? Default = false
+     * @todo Benchmark `use_table`
      */
-    template <typename T, std::size_t n> class Legendre {
+    template <typename T, std::size_t n, bool use_table = false> class Legendre {
       private:
-        std::array<T, n + 1> P; //!< Legendre terms stored here
         std::array<T, n + 1> y; //!< Lookup table for 1+1/i (overkill?)
+        std::array<T, n + 1> P; //!< Legendre terms stored here
       public:
         Legendre() {
             P[0] = 1.0;
-            for (std::size_t i = 1; i < n; ++i) {
-                y[i] = 1.0 + 1.0 / T(i);
+            if constexpr (use_table) {
+                for (std::size_t i = 1; i < n; ++i) {
+                    y[i] = 1.0 + 1.0 / T(i);
+                }
             }
         }
 
@@ -344,23 +348,27 @@ namespace Faunus
             if constexpr (n > 0) {
                 P[1] = x;
                 for (std::size_t i = 1; i < n; ++i) {
-                    P[i + 1] = ((y[i] + 1.0) * x * P[i] - P[i - 1]) / y[i];
+                    if constexpr (use_table) {
+                        P[i + 1] = ((y[i] + 1.0) * x * P[i] - P[i - 1]) / y[i];
+                    } else {
+                        P[i + 1] = ((2.0 + 1.0 / T(i)) * x * P[i] - P[i - 1]) / (1.0 + 1.0 / T(i));
+                    }
                 }
             }
             return P;
         }
     };
 #ifdef DOCTEST_LIBRARY_INCLUDED
-        TEST_CASE("[Faunus] Legendre") {
-            using doctest::Approx;
-            Legendre<double, 3> l;
-            double x = 2.2;
-            auto P = l.eval(x);
-            CHECK(P[0] == Approx(1));
-            CHECK(P[1] == Approx(x));
-            CHECK(P[2] == Approx(0.5 * (3 * x * x - 1)));
-            CHECK(P[3] == Approx(0.5 * (5 * x * x * x - 3 * x)));
-        }
+    TEST_CASE_TEMPLATE("[Faunus] Legendre", LegendreType, Legendre<double, 3, false>, Legendre<double, 3, true>) {
+        using doctest::Approx;
+        LegendreType l;
+        double x = 2.2;
+        auto P = l.eval(x);
+        CHECK(P[0] == Approx(1));
+        CHECK(P[1] == Approx(x));
+        CHECK(P[2] == Approx(0.5 * (3 * x * x - 1)));
+        CHECK(P[3] == Approx(0.5 * (5 * x * x * x - 3 * x)));
+    }
 #endif
 
     /**

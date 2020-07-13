cc2e636ac775b09a0f87a4b22ce2ad563aa13e53
Mikael Lund <Mlund@mac.com>
2020-07-13 11:37:40 +0200

    Refactor Legendre (#303)
    * Legendre refactoring for static size
    
    * Migrate from std::vector to std::array to enable static optimization.
    
    * Make table lookup optional

--- src/auxiliary.h
+++ src/auxiliary.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <array>
 #include <string>
 #include <map>
 #include <regex>
@@ -309,58 +310,65 @@ namespace Faunus
 
 #pragma GCC diagnostic pop
 
-        /**
-         * @brief Evaluate n'th degree Legendre polynomial
-         *
-         * Example:
-         * @code
-         * Legendre<float> l(10);
-         * auto P = l.eval(1.3)
-         * std::cout << P[3]; --> third order value
-         * @endcode
-         *
-         * @author Mikael Lund
-         * @date Canberra 2005-2006
-         * @note Since C++17 there's `std::legendre` but this seems more efficient
-         *       if a range of degrees are needed
-         */
-        template <typename T = double> class Legendre {
-          private:
-            size_t n;         //!< Maximum Legendre order
-            std::vector<T> P; //!< Legendre terms stored here
-            std::vector<T> y; //!< Lookup table for 1+1/i (overkill?)
-          public:
-            /** @brief Construct w. polynomial order>=0 */
-            Legendre(size_t max_order) : n(max_order) {
-                P.resize(n + 1);
-                P[0] = 1.0;
-                y.resize(n + 1);
-                for (size_t i = 1; i < n; i++)
+    /**
+     * @brief Evaluate n'th degree Legendre polynomial
+     *
+     * Example:
+     * @code
+     * Legendre<float, 10> l;
+     * auto P = l.eval(1.3)
+     * std::cout << P[3]; --> third order value
+     * @endcode
+     *
+     * @author Mikael Lund
+     * @date Canberra 2005-2020
+     * @note Since C++17 there's `std::legendre` but this seems more efficient
+     *       if a range of degrees are needed
+     * @tparam T floating point type (double, float, ...)
+     * @tparam n Maximum order to evaluate
+     * @tparam use_table Use lookup table for 1+1/i? Default = false
+     * @todo Benchmark `use_table`
+     */
+    template <typename T, std::size_t n, bool use_table = false> class Legendre {
+      private:
+        std::array<T, n + 1> y; //!< Lookup table for 1+1/i (overkill?)
+        std::array<T, n + 1> P; //!< Legendre terms stored here
+      public:
+        Legendre() {
+            P[0] = 1.0;
+            if constexpr (use_table) {
+                for (std::size_t i = 1; i < n; ++i) {
                     y[i] = 1.0 + 1.0 / T(i);
+                }
             }
+        }
 
-            /** @brief Evaluate polynomials at x */
-            const std::vector<T> &eval(T x) {
-                if (n > 0) {
-                    P[1] = x;
-                    for (size_t i = 1; i < n; ++i) {
+        /** @brief Evaluate polynomials at x */
+        const auto &eval(T x) {
+            if constexpr (n > 0) {
+                P[1] = x;
+                for (std::size_t i = 1; i < n; ++i) {
+                    if constexpr (use_table) {
                         P[i + 1] = ((y[i] + 1.0) * x * P[i] - P[i - 1]) / y[i];
+                    } else {
+                        P[i + 1] = ((2.0 + 1.0 / T(i)) * x * P[i] - P[i - 1]) / (1.0 + 1.0 / T(i));
                     }
                 }
-                return P;
             }
-        };
-#ifdef DOCTEST_LIBRARY_INCLUDED
-        TEST_CASE("[Faunus] Legendre") {
-            using doctest::Approx;
-            Legendre<double> l(3);
-            double x = 2.2;
-            auto P = l.eval(x);
-            CHECK(P[0] == Approx(1));
-            CHECK(P[1] == Approx(x));
-            CHECK(P[2] == Approx(0.5 * (3 * x * x - 1)));
-            CHECK(P[3] == Approx(0.5 * (5 * x * x * x - 3 * x)));
+            return P;
         }
+    };
+#ifdef DOCTEST_LIBRARY_INCLUDED
+    TEST_CASE_TEMPLATE("[Faunus] Legendre", LegendreType, Legendre<double, 3, false>, Legendre<double, 3, true>) {
+        using doctest::Approx;
+        LegendreType l;
+        double x = 2.2;
+        auto P = l.eval(x);
+        CHECK(P[0] == Approx(1));
+        CHECK(P[1] == Approx(x));
+        CHECK(P[2] == Approx(0.5 * (3 * x * x - 1)));
+        CHECK(P[3] == Approx(0.5 * (5 * x * x * x - 3 * x)));
+    }
 #endif
 
     /**

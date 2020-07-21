998b7041df91e9ee31f87389a7d374c7f8453575
Mikael Lund <mlund@mac.com>
2020-07-21 12:10:40 +0200

    Update

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -37,15 +37,15 @@ function(set_project_warnings project_name)
       -Wshadow # warn the user if a variable declaration shadows one from a parent context
       -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
                          # catch hard to track down memory errors
-      -Wold-style-cast # warn for c-style casts
+      #-Wold-style-cast # warn for c-style casts
       -Wcast-align # warn for potential performance problem casts
       -Wunused # warn on anything being unused
       -Woverloaded-virtual # warn if you overload (not override) a virtual function
       -Wpedantic # warn if non-standard C++ is used
-      -Wconversion # warn on type conversions that may lose data
-      -Wsign-conversion # warn on sign conversions
+      #-Wconversion # warn on type conversions that may lose data
+      #-Wsign-conversion # warn on sign conversions
       -Wnull-dereference # warn if a null dereference is detected
-      -Wdouble-promotion # warn if float is implicit promoted to double
+      #-Wdouble-promotion # warn if float is implicit promoted to double
       -Wformat=2 # warn on security issues around functions that format output (ie printf)
   )
 

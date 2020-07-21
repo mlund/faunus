f517bbbf861239774da723b74907cddd41ac203d
Mikael Lund <mlund@mac.com>
2020-07-21 14:14:31 +0200

    Update

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -34,7 +34,7 @@ function(set_project_warnings project_name)
   set(CLANG_WARNINGS
       -Wall
       -Wextra # reasonable and standard
-      -Wshadow # warn the user if a variable declaration shadows one from a parent context
+      #-Wshadow # warn the user if a variable declaration shadows one from a parent context
       -Wnon-virtual-dtor # warn the user if a class with virtual functions has a non-virtual destructor. This helps
                          # catch hard to track down memory errors
       #-Wold-style-cast # warn for c-style casts
@@ -44,6 +44,7 @@ function(set_project_warnings project_name)
       -Wpedantic # warn if non-standard C++ is used
       #-Wconversion # warn on type conversions that may lose data
       #-Wsign-conversion # warn on sign conversions
+      -Wno-sign-compare
       -Wnull-dereference # warn if a null dereference is detected
       #-Wdouble-promotion # warn if float is implicit promoted to double
       -Wformat=2 # warn on security issues around functions that format output (ie printf)

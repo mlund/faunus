3647c88763f5d730c7950a06a612ceb237e1a351
Mikael Lund <mlund@mac.com>
2020-08-03 17:08:16 +0200

    Suppress double/float conversion warning

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -47,7 +47,7 @@ function(set_project_warnings project_name)
             -Wno-sign-compare
             -Wno-unknown-pragmas
             -Wnull-dereference # warn if a null dereference is detected
-            -Wdouble-promotion # warn if float is implicit promoted to double
+            #-Wdouble-promotion # warn if float is implicit promoted to double
             -Wformat=2 # warn on security issues around functions that format output (ie printf)
             )
 

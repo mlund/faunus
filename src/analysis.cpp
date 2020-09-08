2e929b8ba7200cb35dd269f4b1d266e44ea26dca
Richard Chudoba <richard.chudoba@teokem.lu.se>
2020-09-08 15:02:30 +0200

    Conformation id histogram analysis (#326)
    * Allow ipbc keyword for scattering
    
    * Add conformation id histogram analysis
    
    Co-authored-by: Mikael Lund <mlund@mac.com>

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -396,6 +396,25 @@ void VirtualVolume::_to_disk() {
     }
 }
 
+void MolecularConformationID::_sample() {
+    auto molecules = spc.findMolecules(molid, Space::ACTIVE);
+    for (auto &group : molecules) {
+        histogram[group.confid]++;
+    }
+}
+void MolecularConformationID::_to_json(json &j) const { j["histogram"] = histogram; }
+
+MolecularConformationID::MolecularConformationID(const json &j, Space &spc) : spc(spc) {
+    from_json(j);
+    name = "moleculeconformation";
+    const std::string molname = j.at("molecule");
+    if (auto it = findName(Faunus::molecules, molname); it == molecules.end()) {
+        throw std::runtime_error("unknown molecule '" + molname + "'");
+    } else {
+        molid = it->id();
+    }
+}
+
 void QRtraj::_sample() { write_to_file(); }
 
 void QRtraj::_to_json(json &j) const { j = {{"file", filename}}; }
@@ -461,6 +480,8 @@ CombinedAnalysis::CombinedAnalysis(const json &j, Space &spc, Energy::Hamiltonia
                             emplace_back<AtomInertia>(it.value(), spc);
                         else if (it.key() == "inertia")
                             emplace_back<InertiaTensor>(it.value(), spc);
+                        else if (it.key() == "moleculeconformation")
+                            emplace_back<MolecularConformationID>(it.value(), spc);
                         else if (it.key() == "multipolemoments")
                             emplace_back<MultipoleMoments>(it.value(), spc);
                         else if (it.key() == "multipoledist")

772d097f535aeadf1c35b65943988c45110407f8
Mikael Lund <mlund@mac.com>
2021-03-28 14:45:15 +0200

    Use Eigen::Ones() over Point(1,1,1)

--- src/analysis.cpp
+++ src/analysis.cpp
@@ -872,7 +872,7 @@ SanityCheck::SanityCheck(const json& j, Space& spc) : Analysisbase(spc, "sanity"
 void AtomRDF::sampleDistance(const Point& position1, const Point& position2) {
     const auto distance = spc.geo.vdist(position1, position2);
     if (slicedir.sum() > 0) {
-        if (distance.cwiseProduct((Point(1.0,1.0,1.0)-slicedir.cast<double>()).cwiseAbs()).norm() < thickness) {
+        if (distance.cwiseProduct((Point::Ones() - slicedir.cast<double>()).cwiseAbs()).norm() < thickness) {
             histogram(distance.cwiseProduct(slicedir.cast<double>()).norm())++;
         }
     } else {

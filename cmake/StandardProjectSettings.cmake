1ed9ca7694ac7e042a96d4aa0a145ee1e4e1bfef
Mikael Lund <mikael.lund@teokem.lu.se>
2021-04-19 12:36:28 +0200

    Stabilise build sequence
    - ENABLE_PYTHON=off by default
    - place SDL/cppsid stuff inside ENABLE_SID condition
    - github action use Debug build
    - add python package check

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -2,7 +2,7 @@
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   message(STATUS "Setting build type to 'RelWithDebInfo' as none was specified.")
   set(CMAKE_BUILD_TYPE
-      RelWithDebInfo
+      Release
       CACHE STRING "Choose the type of build." FORCE)
   # Set the possible values of build type for cmake-gui, ccmake
   set_property(

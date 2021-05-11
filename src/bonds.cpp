2a6635a37e6d0e836ae00ecfa0555d1f7958a631
Mikael Lund <mikael.lund@teokem.lu.se>
2021-05-11 19:14:14 +0200

    Update

--- src/bonds.cpp
+++ src/bonds.cpp
@@ -213,9 +213,9 @@ void HarmonicTorsion::setEnergyFunction(const ParticleVector& particles) {
             const auto inverse_sine_angle = 1.0 / std::sqrt(std::fabs(1.0 - cosine_angle * cosine_angle));
             const auto angle = std::acos(cosine_angle);
             const auto prefactor = 2.0 * half_force_constant * (angle - equilibrium_angle) * inverse_sine_angle;
-            const Point force0 = prefactor * inverse_norm1 * (vec2 - vec1 * cosine_angle);
-            const Point force2 = prefactor * inverse_norm2 * (vec1 - vec2 * cosine_angle);
-            const Point force1 = -(force0 + force2); // no net force
+            Point force0 = prefactor * inverse_norm1 * (vec2 - vec1 * cosine_angle);
+            Point force2 = prefactor * inverse_norm2 * (vec1 - vec2 * cosine_angle);
+            Point force1 = -(force0 + force2); // no net force
             return {{indices[0], force0}, {indices[1], force1}, {indices[2], force2}};
         } else { // @todo which one of these two are fastest?
             const Point ba = calculateDistance(particles[indices[0]].pos, particles[indices[1]].pos);
@@ -225,9 +225,9 @@ void HarmonicTorsion::setEnergyFunction(const ParticleVector& particles) {
             const auto angle = std::acos(ba.dot(bc) * inverse_norm_ba * inverse_norm_bc);
             const auto forcemagnitude = -2.0 * half_force_constant * (angle - equilibrium_angle);
             const Point plane_babc = ba.cross(bc).eval();
-            const Point force0 = (forcemagnitude * inverse_norm_ba * ba.cross(plane_babc).normalized());
-            const Point force2 = (forcemagnitude * inverse_norm_bc * -bc.cross(plane_babc).normalized());
-            const Point force1 = -(force0 + force2); // Newton's third law
+            Point force0 = (forcemagnitude * inverse_norm_ba * ba.cross(plane_babc).normalized());
+            Point force2 = (forcemagnitude * inverse_norm_bc * -bc.cross(plane_babc).normalized());
+            Point force1 = -(force0 + force2); // Newton's third law
             return {{indices[0], force0}, {indices[1], force1}, {indices[2], force2}};
         }
     };

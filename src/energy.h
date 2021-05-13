91227be2fba3a9be16ad9b798536be89856dae22
Mikael Lund <mlund@mac.com>
2021-05-13 10:36:23 +0200

    Update

--- src/energy.h
+++ src/energy.h
@@ -1192,36 +1192,43 @@ class Nonbonded : public Energybase {
      * @todo A stub. Change to reflect only active particle, see Space::activeParticles().
      */
     void force(std::vector<Point> &forces) override {
-        auto has_internal_force = [](const auto& group) {
-            if (group.traits().rigid || !group.traits().compressible) {
-                return false;
-            }
-            return true;
-        };
         auto update_force = [&](const Particle& particle1, const Particle& particle2) {
             auto force = pair_energy.force(particle1, particle2);
-            const auto first_particle_addesss = std::addressof(spc.p.front());
-            const auto index1 = std::addressof(particle1) - first_particle_addesss;
-            const auto index2 = std::addressof(particle2) - first_particle_addesss;
+            const auto first_particle_addresss = std::addressof(spc.p.front());
+            const auto index1 = std::addressof(particle1) - first_particle_addresss;
+            const auto index2 = std::addressof(particle2) - first_particle_addresss;
             forces[index1] += force;
             forces[index2] -= force;
         };
-        // intra-molecular forces
-        for (const Space::Tgroup& group : spc.groups | ranges::cpp20::views::filter(has_internal_force)) {
+
+        // forces within a single group
+        auto internal_forces = [&](const Space::Tgroup& group) {
             for (auto particle1 = group.begin(); particle1 != group.end(); ++particle1) {
                 for (auto particle2 = particle1; ++particle2 != group.end();) {
                     update_force(*particle1, *particle2);
                 }
             }
-        }
-        // inter-molecular forces
+        };
+        auto has_internal_force = [](const auto& group) {
+          if (group.traits().rigid || !group.traits().compressible) {
+              return false;
+          }
+          return true;
+        };
+        auto compressible_groups = spc.groups | ranges::cpp20::views::filter(has_internal_force);
+        std::for_each(compressible_groups.begin(), compressible_groups.end(), internal_forces);
+
+        // forces between groups
+        auto intermolecular_forces = [&](const Space::Tgroup& group1, const Space::Tgroup& group2) {
+            for (const auto& particle_i : group1) {
+                for (const auto& particle_j : group2) {
+                    update_force(particle_i, particle_j);
+                }
+            }
+        };
         for (auto group_i = spc.groups.cbegin(); group_i != spc.groups.cend(); ++group_i) {
             for (auto group_j = group_i; ++group_j != spc.groups.cend();) {
-                for (const auto& particle1 : *group_i) {
-                    for (const auto& particle2 : *group_j) {
-                        update_force(particle1, particle2);
-                    }
-                }
+                intermolecular_forces(*group_i, *group_j);
             }
         }
         if constexpr (false) { // this code is disabled!

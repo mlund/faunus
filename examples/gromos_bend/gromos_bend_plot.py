ff815cc4dfaf2fbc5da63a2896f20ebdfa5829de
Stefan Hervø-Hansen <stefan.hervo_hansen@teokem.lu.se>
2021-05-25 11:20:52 +0200

    Improved readability and updated snake_case usage.
    *Rename all bonded force examples to use snake_case.
    *Updated `examples/CMakeLists.txt` to reflect new names.
    *Miscellaneous clean-up related to bonded examples.

--- examples/gromos_bend/gromos_bend_plot.py
+++ examples/gromos_bend/gromos_bend_plot.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-mc = np.loadtxt('dihedral-mc-rdf.dat', unpack=True, usecols=(0,1))
-ld = np.loadtxt('dihedral-md-rdf.dat', unpack=True, usecols=(0,1))
+mc = np.loadtxt('gromos_bend_mc_rdf.dat', unpack=True, usecols=(0,1))
+ld = np.loadtxt('gromos_bend_md_rdf.dat', unpack=True, usecols=(0,1))
 
 plt.plot(mc[0], mc[1], label='Monte Carlo', linewidth=10, alpha=0.3)
 plt.plot(ld[0], ld[1], label='Langevin', linewidth=2)
@@ -13,8 +13,7 @@ if os.path.isfile('rdf.dat'):
     test = np.loadtxt('rdf.dat', unpack=True, usecols=(0,1))
     plt.plot(test[0], test[1], label='Test', linewidth=2)
 
-plt.xlim(0, 5)
 plt.legend(loc=0, frameon=False, fontsize=10)
 plt.xlabel('Distance, $r$ (Å)', fontsize=14)
 plt.ylabel('$g(r)$', fontsize=14)
-plt.savefig('dihedral-plot.png')
+plt.savefig('gromos_bend_plot.png')
--- examples/improper_dihedral/improper_dihedral_plot.py
+++ examples/improper_dihedral/improper_dihedral_plot.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-mc = np.loadtxt('improper-dihedral-mc-rdf.dat', unpack=True, usecols=(0,1))
-ld = np.loadtxt('improper-dihedral-md-rdf.dat', unpack=True, usecols=(0,1))
+mc = np.loadtxt('improper_dihedral_mc_rdf.dat', unpack=True, usecols=(0,1))
+ld = np.loadtxt('improper_dihedral_md_rdf.dat', unpack=True, usecols=(0,1))
 
 plt.plot(mc[0], mc[1], label='Monte Carlo', linewidth=10, alpha=0.3)
 plt.plot(ld[0], ld[1], label='Langevin', linewidth=2)
@@ -17,4 +17,4 @@ plt.xlim(0, 7)
 plt.legend(loc=0, frameon=False, fontsize=10)
 plt.xlabel('Distance, $r$ (Å)', fontsize=14)
 plt.ylabel('$g(r)$', fontsize=14)
-plt.savefig('improper-dihedral-plot.png')
+plt.savefig('improper_dihedral_plot.png')

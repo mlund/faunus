a570f7509574c808382245bede89b158ed684827
Marcel Bargull <mbargull@users.noreply.github.com>
2023-11-02 14:17:25 +0100

    Use upstream ruamel.yaml instead of ruamel_yaml (#435)
    * Use upstream ruamel.yaml instead of ruamel_yaml
    
    The ruamel_yaml package was created by Anaconda as a workaround for
    namespace package issues a long time ago.
    By now it is not needed anymore and ruamel.yaml is available from
    Anaconda's "defaults" channel as well as conda-forge.
    Conda-forge will stop (re-)building (the severly outdated) ruamel_yaml
    package; tracking issue:
    https://github.com/conda-forge/ruamel_yaml-feedstock/issues/107
    
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>
    
    * Add compatibility for ruamel.yaml=0.18
    
    ruamel.yaml=0.18 removed the top-level load, dump, etc. functions.
    For details see https://pypi.org/project/ruamel.yaml/0.18.4/ .
    
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>
    
    ---------
    
    Signed-off-by: Marcel Bargull <marcel.bargull@udo.edu>

--- scripts/extractsids.py
+++ scripts/extractsids.py
@@ -10,7 +10,13 @@ if sys.version_info < (3, 0):
     sys.exit(1)
 
 import os, json, urllib.request
-import ruamel_yaml as yaml
+try:
+    import ruamel.yaml
+except ImportError:
+    from yaml import safe_load as yaml_safe_load
+else:
+    def yaml_safe_load(stream):
+        return ruamel.yaml.YAML(typ="safe").load(stream)
 
 if len(sys.argv)!=2:
     print("usage: {} music.yml".format(sys.argv[0]))
@@ -19,7 +25,7 @@ if len(sys.argv)!=2:
 filename = sys.argv[1]
 
 with open(filename) as f:
-    sidlist = yaml.safe_load(f)
+    sidlist = yaml_safe_load(f)
     server = sidlist['server'] + '/'
     dstdir = 'sids/'
     if not os.path.exists(dstdir):

4212e7d5d3cf1d893f748e1b064e00d37d938098
mikael <mlund@mac.com>
2024-06-16 21:21:54 +0200

    Tidy average

--- src/average.cpp
+++ src/average.cpp
@@ -7,23 +7,23 @@ TEST_CASE("[Faunus] Average") {
     Faunus::Average<double> a;
     a += 1.0;
     a += 2.0;
-    CHECK(a.size() == 2);
-    CHECK(a.avg() == 1.5);
-    CHECK(static_cast<double>(a) == 1.5); // conversion to double
+    CHECK_EQ(a.size(), 2);
+    CHECK_EQ(a.avg(), 1.5);
+    CHECK_EQ(static_cast<double>(a), 1.5); // conversion to double
 
     auto b = a;        // copy
     CHECK(!b.empty()); // check not empty()
-    CHECK(a == b);
+    CHECK_EQ(a, b);
     CHECK_EQ(a, b);
     b.clear();        // reset all data
     CHECK(b.empty()); // check empty()
     b += 2.0;
     b += 3.0;
-    CHECK(a < b); // a.avg() < b.avg()
-    CHECK((a + b).avg() == doctest::Approx(2.0));
+    CHECK((a < b)); // a.avg() < b.avg()
+    CHECK_EQ((a + b).avg(), doctest::Approx(2.0));
 
     b = 1.0; // assign from double
-    CHECK(b.size() == 1);
+    CHECK_EQ(b.size(), 1);
 
     SUBCASE("overflow") {
         Average<double, std::uint8_t> a;
@@ -31,7 +31,7 @@ TEST_CASE("[Faunus] Average") {
         for (std::uint8_t i = 0; i < max_number_of_samples; i++) {
             a += 1.0;
         }
-        CHECK(a.size() == max_number_of_samples);
+        CHECK_EQ(a.size(), max_number_of_samples);
         CHECK_THROWS(a += 1.0);
     }
 }
@@ -40,23 +40,23 @@ TEST_CASE("[Faunus] AverageStd") {
     Faunus::AverageStdev<double> a;
     a += 1.0;
     a += 2.0;
-    CHECK(a.size() == 2);
-    CHECK(a.rms() == doctest::Approx(1.5811388301));
-    CHECK(a.avg() == 1.5);
-    CHECK(static_cast<double>(a) == 1.5); // conversion to double
+    CHECK_EQ(a.size(), 2);
+    CHECK_EQ(a.rms(), doctest::Approx(1.5811388301));
+    CHECK_EQ(a.avg(), 1.5);
+    CHECK_EQ(static_cast<double>(a), 1.5); // conversion to double
 
     auto b = a;        // copy
     CHECK(!b.empty()); // check not empty()
-    CHECK(a == b);
+    CHECK_EQ(a, b);
     CHECK_EQ(a, b);
     b.clear();        // reset all data
     CHECK(b.empty()); // check empty()
     b += 2.0;
     b += 3.0;
-    CHECK(a < b); // a.avg() < b.avg()
+    CHECK((a < b)); // a.avg() < b.avg()
 
     b = 1.0; // assign from double
-    CHECK(b.size() == 1);
+    CHECK_EQ(b.size(), 1);
 }
 
 TEST_CASE("[Faunus] AverageObj") {
@@ -64,8 +64,8 @@ TEST_CASE("[Faunus] AverageObj") {
     Faunus::AverageObj<double> a;
     a += 1.0;
     a += 2.0;
-    CHECK(a.avg() == Approx(1.5));
-    CHECK(a == Approx(1.5)); // implicit conversion to double
+    CHECK_EQ(a.avg(), Approx(1.5));
+    CHECK_EQ(a, Approx(1.5)); // implicit conversion to double
 
     struct MyClass {
         double x;
@@ -86,7 +86,7 @@ TEST_CASE("[Faunus] AverageObj") {
     k2.x = 20.0;
     b += k1;
     b += k2;
-    CHECK(b.avg().x == Approx(15.0));
+    CHECK_EQ(b.avg().x, Approx(15.0));
 }
 
 } // namespace Faunus

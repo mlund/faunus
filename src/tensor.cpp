b2d301c301374e47a8a560095097f8ad024ae250
mikael <mlund@mac.com>
2024-06-16 21:49:43 +0200

    Tidy tensor

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -17,7 +17,7 @@ void Tensor::rotate(const Tensor::base &rotation_matrix) {
     (*this) = rotation_matrix * (*this) * rotation_matrix.transpose();
 }
 
-void Tensor::eye() { *this = base::Identity(3, 3); }
+[[maybe_unused]] void Tensor::eye() { *this = base::Identity(3, 3); }
 
 void to_json(nlohmann::json &j, const Tensor &t) { j = {t(0, 0), t(0, 1), t(0, 2), t(1, 1), t(1, 2), t(2, 2)}; }
 
@@ -35,17 +35,17 @@ TEST_CASE("[Faunus] Tensor") {
     using namespace nlohmann;
     Tensor Q1 = Tensor(1, 2, 3, 4, 5, 6);
     Tensor Q2 = json(Q1);        // Q1 --> json --> Q2
-    CHECK(json(Q1) == json(Q2)); // Q1 --> json == json <-- Q2 ?
-    CHECK(Q2 == Tensor(1, 2, 3, 4, 5, 6));
+    CHECK_EQ(json(Q1), json(Q2)); // Q1 --> json == json <-- Q2 ?
+    CHECK_EQ(Q2, Tensor(1, 2, 3, 4, 5, 6));
 
     auto m = Eigen::AngleAxisd(pc::pi / 2, Point(0, 1, 0)).toRotationMatrix();
     Q1.rotate(m);
-    CHECK(Q1(0, 0) == Approx(6));
-    CHECK(Q1(0, 1) == Approx(5));
-    CHECK(Q1(0, 2) == Approx(-3));
-    CHECK(Q1(1, 1) == Approx(4));
-    CHECK(Q1(1, 2) == Approx(-2));
-    CHECK(Q1(2, 2) == Approx(1));
+    CHECK_EQ(Q1(0, 0), Approx(6));
+    CHECK_EQ(Q1(0, 1), Approx(5));
+    CHECK_EQ(Q1(0, 2), Approx(-3));
+    CHECK_EQ(Q1(1, 1), Approx(4));
+    CHECK_EQ(Q1(1, 2), Approx(-2));
+    CHECK_EQ(Q1(2, 2), Approx(1));
 }
 
 TEST_SUITE_END();
